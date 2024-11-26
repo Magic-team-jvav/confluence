c84fea47ece0ee1f7b7d309aa3f0690e526fe615
westernat <westernat233@163.com>
2024-11-26 16:00:53 +0800

    小改

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/LootModifierManagerMixin.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/LootModifierManagerMixin.java
@@ -1,26 +1,22 @@
 package org.confluence.mod.mixin;
 
-import com.google.common.collect.ImmutableMap;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.google.gson.JsonElement;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.util.profiling.ProfilerFiller;
 import net.neoforged.neoforge.common.loot.LootModifierManager;
 import org.confluence.terra_curio.TerraCurio;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
 @Mixin(value = LootModifierManager.class, remap = false, priority = 900)
 public abstract class LootModifierManagerMixin {
-    @ModifyExpressionValue(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at= @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableMap$Builder;build()Lcom/google/common/collect/ImmutableMap;"))
-    private ImmutableMap<ResourceLocation, IGlobalLootModifier> removeTerraCurio(ImmutableMap<ResourceLocation, IGlobalLootModifier> original) {
-        ImmutableMap.Builder<ResourceLocation, IGlobalLootModifier> builder = ImmutableMap.builder();
-        for (Map.Entry<ResourceLocation, IGlobalLootModifier> entry : original.entrySet()) {
-            if (!entry.getKey().getNamespace().equals(TerraCurio.MODID)) {
-                builder.put(entry);
-            }
-        }
-        return builder.build();
+    @Inject(method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V", at = @At("HEAD"))
+    private void removeTerraCurio(Map<ResourceLocation, JsonElement> resourceList, ResourceManager resourceManagerIn, ProfilerFiller profilerIn, CallbackInfo ci) {
+        resourceList.entrySet().removeIf(entry -> entry.getKey().getNamespace().equals(TerraCurio.MODID));
     }
 }

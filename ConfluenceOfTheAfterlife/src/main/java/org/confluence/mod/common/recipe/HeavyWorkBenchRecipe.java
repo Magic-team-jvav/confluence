3c0df50b056ed30d053a3efba1027aa988746955
westernat <westernat233@163.com>
2024-12-22 13:33:41 +0800

    重型工作台（应该是做完了

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/recipe/HeavyWorkBenchRecipe.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/recipe/HeavyWorkBenchRecipe.java
@@ -5,20 +5,32 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.item.crafting.ShapedRecipePattern;
+import net.minecraft.world.level.Level;
 import org.confluence.mod.common.init.ModRecipes;
 import org.confluence.mod.common.init.block.FunctionalBlocks;
 import org.confluence.terra_curio.common.recipe.AbstractAmountRecipe;
 import org.jetbrains.annotations.NotNull;
 
 public class HeavyWorkBenchRecipe extends AbstractAmountRecipe {
-    public final ShapedRecipePattern pattern;
+    public final AmountShapedRecipePattern pattern;
 
-    protected HeavyWorkBenchRecipe(ShapedRecipePattern pattern, ItemStack pResult) {
+    protected HeavyWorkBenchRecipe(ItemStack pResult, AmountShapedRecipePattern pattern) {
         super(pResult, pattern.ingredients());
         this.pattern = pattern;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int width, int height) {
+        return width >= pattern.width() && height >= pattern.height();
+    }
+
+    @Override
+    public boolean matches(@NotNull RecipeInput pContainer, @NotNull Level pLevel) {
+        return pattern.matches(pContainer, 4, 4);
     }
 
     @Override
@@ -48,8 +60,8 @@ public class HeavyWorkBenchRecipe extends AbstractAmountRecipe {
 
     public static class Serializer implements RecipeSerializer<HeavyWorkBenchRecipe> {
         public static final MapCodec<HeavyWorkBenchRecipe> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-                ShapedRecipePattern.MAP_CODEC.forGetter(recipe -> recipe.pattern),
-                ItemStack.STRICT_CODEC.fieldOf("result").forGetter(recipe -> recipe.result)
+                ItemStack.STRICT_CODEC.fieldOf("result").forGetter(recipe -> recipe.result),
+                AmountShapedRecipePattern.MAP_CODEC.forGetter(recipe -> recipe.pattern)
         ).apply(instance, HeavyWorkBenchRecipe::new));
         public static final StreamCodec<RegistryFriendlyByteBuf, HeavyWorkBenchRecipe> STREAM_CODEC = StreamCodec.of(Serializer::toNetwork, Serializer::fromNetwork);
 
@@ -64,14 +76,14 @@ public class HeavyWorkBenchRecipe extends AbstractAmountRecipe {
         }
 
         private static HeavyWorkBenchRecipe fromNetwork(RegistryFriendlyByteBuf buffer) {
-            ShapedRecipePattern shapedrecipepattern = ShapedRecipePattern.STREAM_CODEC.decode(buffer);
             ItemStack itemstack = ItemStack.STREAM_CODEC.decode(buffer);
-            return new HeavyWorkBenchRecipe(shapedrecipepattern, itemstack);
+            AmountShapedRecipePattern shapedrecipepattern = AmountShapedRecipePattern.STREAM_CODEC.decode(buffer);
+            return new HeavyWorkBenchRecipe(itemstack, shapedrecipepattern);
         }
 
         private static void toNetwork(RegistryFriendlyByteBuf buffer, HeavyWorkBenchRecipe recipe) {
-            ShapedRecipePattern.STREAM_CODEC.encode(buffer, recipe.pattern);
             ItemStack.STREAM_CODEC.encode(buffer, recipe.result);
+            ShapedRecipePattern.STREAM_CODEC.encode(buffer, recipe.pattern);
         }
     }
 }

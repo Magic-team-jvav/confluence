4577c5e55fca6d6541cf98d6a043e2522e128b2d
westernat <westernat233@163.com>
2024-12-29 23:22:58 +0800

    改为Shadow player

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/client/MinecraftMixin.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/client/MinecraftMixin.java
@@ -2,6 +2,7 @@ package org.confluence.mod.mixin.client;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.LevelLoadingScreen;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.server.WorldStem;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.world.entity.Entity;
@@ -13,30 +14,35 @@ import org.confluence.mod.common.init.ModEffects;
 import org.confluence.mod.mixed.ILevelLoadingScreen;
 import org.confluence.mod.mixed.IWorldOptions;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import javax.annotation.Nullable;
 import java.time.Instant;
 
 import static org.confluence.mod.client.ModKeyBindings.SHOW_DETAIL_SPECULAR;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
+    @Shadow
+    @Nullable
+    public LocalPlayer player;
+
     @Inject(method = "doWorldLoad", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;setScreen(Lnet/minecraft/client/gui/screens/Screen;)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
     private void setSecretFlag(LevelStorageSource.LevelStorageAccess levelStorage, PackRepository packRepository, WorldStem worldStem, boolean newWorld, CallbackInfo ci, Instant instant, LevelLoadingScreen levelloadingscreen) {
         ((ILevelLoadingScreen) levelloadingscreen).confluence$setSecretFlag(((IWorldOptions) worldStem.worldData().worldGenOptions()).confluence$getSecretFlag());
     }
 
-
     @Inject(method = "shouldEntityAppearGlowing", at = @At(value = "HEAD"), cancellable = true)
     public void changeGlowOutline(Entity entity, CallbackInfoReturnable<Boolean> cir) {
-        if (Minecraft.getInstance().player != null) {
+        if (player != null) {
             GlowingHelper helper = GlowingHelper.INSTANCE;
             //狩猎药水
-            if (Minecraft.getInstance().player.hasEffect(ModEffects.HUNTER)) {
+            if (player.hasEffect(ModEffects.HUNTER)) {
 
                 //自定义类别 中立生物不计入其中
                 for (var n : helper.hunterCatalog)
@@ -66,11 +72,10 @@ public abstract class MinecraftMixin {
                 }
 
 
-
             }
 
             //危险感知药水
-            if (Minecraft.getInstance().player.hasEffect(ModEffects.DANGER_SENSE)) {
+            if (player.hasEffect(ModEffects.DANGER_SENSE)) {
                 for (var n : helper.dangerCatalog) {
                     if (n.isAssignableFrom(entity.getClass())) {
                         if (SHOW_DETAIL_SPECULAR.get().isDown()) {

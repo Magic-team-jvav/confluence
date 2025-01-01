037dae7a98886744bdfefd7c18cacfd0d3c1cbde
westernat <westernat233@163.com>
2025-01-01 17:30:34 +0800

    时装栏有用

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/util/ModUtils.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/util/ModUtils.java
@@ -14,6 +14,7 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -481,5 +482,13 @@ public final class ModUtils {
     public static boolean isDamageFromConfluenceWeapon(DamageSource source){
         ItemStack weaponItem = source.getWeaponItem();
         return weaponItem != null && weaponItem.getItem() instanceof Immunity; // TODO: 打表
+    }
+
+    public static int getSlotIndex(EquipmentSlot slot) {
+        if (slot == EquipmentSlot.HEAD) return 0;
+        if (slot == EquipmentSlot.CHEST) return 1;
+        if (slot == EquipmentSlot.LEGS) return 2;
+        if (slot == EquipmentSlot.FEET) return 3;
+        return -1;
     }
 }

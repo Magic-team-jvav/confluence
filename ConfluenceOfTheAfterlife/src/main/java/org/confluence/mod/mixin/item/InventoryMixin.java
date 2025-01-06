241f8910c37dc2d3151481a833f4a7038d5cf7cb
westernat <westernat233@163.com>
2025-01-06 15:44:07 +0800

    自动堆叠金币

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/item/InventoryMixin.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/item/InventoryMixin.java
@@ -2,10 +2,12 @@ package org.confluence.mod.mixin.item;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.confluence.mod.common.attachment.ExtraInventory;
 import org.confluence.mod.common.init.ModAttachmentTypes;
 import org.confluence.mod.common.init.ModTags;
+import org.confluence.mod.common.init.item.ModItems;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,6 +15,8 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.function.Consumer;
 
 import static org.confluence.mod.common.attachment.ExtraInventory.*;
 
@@ -28,23 +32,46 @@ public abstract class InventoryMixin {
     @Inject(method = "add(ILnet/minecraft/world/item/ItemStack;)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isDamaged()Z"), cancellable = true)
     private void add2Extra(int slot, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         if (stack.is(ModTags.Items.COINS)) {
-            if (confluence$insert2Extra(COINS_START, SIZE_COINS, stack)) {
+            ExtraInventory extraInventory = player.getData(ModAttachmentTypes.EXTRA_INVENTORY);
+            if (confluence$insert2Extra(COINS_START, SIZE_COINS, extraInventory, stack, extraInventory1 -> {
+                for (int i = 0; i < SIZE_COINS; i++) {
+                    ItemStack coins = extraInventory.getCoins(i);
+                    if (coins.getCount() == coins.getMaxStackSize()) {
+                        Item coin = coins.getItem();
+                        ItemStack itemStack = null;
+                        if (coin == ModItems.COPPER_COIN.get()) {
+                            itemStack = ModItems.SILVER_COIN.get().getDefaultInstance();
+                        } else if (coin == ModItems.SILVER_COIN.get()) {
+                            itemStack = ModItems.GOLDEN_COIN.get().getDefaultInstance();
+                        } else if (coin == ModItems.GOLDEN_COIN.get()) {
+                            itemStack = ModItems.PLATINUM_COIN.get().getDefaultInstance();
+                        } else if (coin == ModItems.PLATINUM_COIN.get()) {
+                            itemStack = ModItems.EMERALD_COIN.get().getDefaultInstance();
+                        }
+                        if (itemStack != null) {
+                            extraInventory1.setItem(COINS_START + i, ItemStack.EMPTY);
+                            confluence$insert2Extra(COINS_START, SIZE_COINS, extraInventory1, itemStack, extraInventory2 -> {});
+                        }
+                    }
+                }
+            })) {
                 cir.setReturnValue(true);
             }
         } else if (stack.is(ModTags.Items.AMMO)) {
-            if (confluence$insert2Extra(AMMO_START, SIZE_AMMO, stack)) {
+            ExtraInventory extraInventory = player.getData(ModAttachmentTypes.EXTRA_INVENTORY);
+            if (confluence$insert2Extra(AMMO_START, SIZE_AMMO, extraInventory, stack, extraInventory2 -> {})) {
                 cir.setReturnValue(true);
             }
         }
     }
 
     @Unique
-    private boolean confluence$insert2Extra(int head, int size, ItemStack stack) {
-        ExtraInventory extraInventory = player.getData(ModAttachmentTypes.EXTRA_INVENTORY);
+    private boolean confluence$insert2Extra(int head, int size, ExtraInventory extraInventory, ItemStack stack, Consumer<ExtraInventory> consumer) {
         int i;
         do {
             i = stack.getCount();
             stack.setCount(confluence$addResource2Extra(head, size, extraInventory, stack));
+            consumer.accept(extraInventory);
         } while (!stack.isEmpty() && stack.getCount() < i);
 
         if (stack.getCount() == i && player.hasInfiniteMaterials()) {

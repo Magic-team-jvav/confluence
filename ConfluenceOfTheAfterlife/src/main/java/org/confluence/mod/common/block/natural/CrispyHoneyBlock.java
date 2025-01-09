ad631b46960b468d07bc56d05dc6b5f296950df9
voila <voila1106@gmail.com>
2025-01-09 13:48:19 +0800

    松脆蜂蜜块烫人改成秘密种子特性

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/block/natural/CrispyHoneyBlock.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/block/natural/CrispyHoneyBlock.java
@@ -1,6 +1,7 @@
 package org.confluence.mod.common.block.natural;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -11,10 +12,10 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MapColor;
+import org.confluence.mod.common.worldgen.secret_seed.ModSecretSeeds;
 
 import javax.annotation.Nullable;
 
-// TODO: 这些是get fixed boi和for the worthy的特性
 public class CrispyHoneyBlock extends Block {
     public CrispyHoneyBlock() {
         super(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BROWN).strength(1.2F));
@@ -22,13 +23,17 @@ public class CrispyHoneyBlock extends Block {
 
     public void playerDestroy(Level pLevel, Player pPlayer, BlockPos pPos, BlockState pState, @Nullable BlockEntity pTe, ItemStack pStack) {
         super.playerDestroy(pLevel, pPlayer, pPos, pState, pTe, pStack);
-        if (pLevel.random.nextBoolean()) {
+        if(pLevel instanceof ServerLevel serverLevel
+            && (ModSecretSeeds.GET_FIXED_BOI.match(serverLevel) || ModSecretSeeds.FOR_THE_WORTHY.match(serverLevel))
+            && pLevel.random.nextBoolean()){
             pLevel.setBlockAndUpdate(pPos, Blocks.LAVA.defaultBlockState());
         }
     }
 
     @Override
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
-        pEntity.igniteForSeconds(5.0F);
+        if(pLevel instanceof ServerLevel serverLevel && (ModSecretSeeds.GET_FIXED_BOI.match(serverLevel) || ModSecretSeeds.FOR_THE_WORTHY.match(serverLevel))){
+            pEntity.igniteForSeconds(5.0F);
+        }
     }
 }

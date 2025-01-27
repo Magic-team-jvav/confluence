1179c58fa92627f8d9a56bb0114288999875d777
westernat <westernat233@163.com>
2025-01-27 18:10:27 +0800

    优化大法好

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/MinecraftServerMixin.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/mixin/MinecraftServerMixin.java
@@ -1,8 +1,12 @@
 package org.confluence.mod.mixin;
 
-import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
-import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
+import com.mojang.datafixers.DataFixer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.Services;
+import net.minecraft.server.WorldStem;
+import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
+import net.minecraft.server.packs.repository.PackRepository;
+import net.minecraft.world.level.storage.LevelStorageSource;
 import net.minecraft.world.level.storage.WorldData;
 import org.confluence.mod.common.worldgen.secret_seed.SecretSeed;
 import org.confluence.mod.mixed.IMinecraftServer;
@@ -10,18 +14,27 @@ import org.confluence.mod.mixed.IWorldOptions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.function.Predicate;
+import java.net.Proxy;
 
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerMixin implements IMinecraftServer {
-    @Shadow public abstract WorldData getWorldData();
+    @Shadow
+    public abstract WorldData getWorldData();
 
     @Unique
-    private final Object2BooleanMap<SecretSeed> confluence$secretSeedsCache = new Object2BooleanOpenHashMap<>();
+    private long confluence$secretFlag;
 
     @Override
     public boolean confluence$cacheSecretSeeds(SecretSeed secretSeed) {
-        return confluence$secretSeedsCache.computeIfAbsent(secretSeed, (Predicate<? super SecretSeed>) seed -> seed.match(((IWorldOptions) getWorldData().worldGenOptions()).confluence$getSecretFlag()));
+        return secretSeed.match(confluence$secretFlag);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void initialize(Thread serverThread, LevelStorageSource.LevelStorageAccess storageSource, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer fixerUpper, Services services, ChunkProgressListenerFactory progressListenerFactory, CallbackInfo ci) {
+        this.confluence$secretFlag = ((IWorldOptions) getWorldData().worldGenOptions()).confluence$getSecretFlag();
     }
 }

469dc2f62a7947fc360874b8d37babbb0c3c70a2
westernat <westernat233@163.com>
2025-01-29 18:29:25 +0800

    骗jade

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/integration/jade/ModJadePlugin.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/integration/jade/ModJadePlugin.java
@@ -2,11 +2,13 @@ package org.confluence.mod.integration.jade;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Blocks;
 import org.confluence.mod.common.block.functional.AbstractMechanicalBlock;
 import org.confluence.mod.common.block.functional.BehaviourPressurePlateBlock;
 import org.confluence.mod.common.block.functional.DeathChestBlock;
 import org.confluence.mod.common.block.functional.SignalPressurePlateBlock;
 import org.confluence.mod.common.entity.TreasureBagItemEntity;
+import org.confluence.mod.common.init.block.FunctionalBlocks;
 import snownee.jade.api.*;
 
 @WailaPlugin
@@ -26,10 +28,14 @@ public class ModJadePlugin implements IWailaPlugin {
         registration.addRayTraceCallback((hitResult, accessor, originalAccessor) -> {
             if (accessor instanceof BlockAccessor blockAccessor) {
                 Player player = accessor.getPlayer();
-                if (!player.isCreative() && blockAccessor.getBlockEntity() instanceof DeathChestBlock.Entity entity) { // 隐藏死人箱
+                if (player.isCreative()) return accessor;
+                if (blockAccessor.getBlockEntity() instanceof DeathChestBlock.Entity entity) { // 隐藏死人箱
                     CompoundTag serverData = blockAccessor.getServerData();
                     serverData.putString("givenName", "{\"translate\":\"block.confluence.base_chest_block." + entity.variant.getSerializedName() + "\"}");
                     return registration.blockAccessor().from(blockAccessor).serverData(serverData).build();
+                }
+                if (blockAccessor.getBlock() == FunctionalBlocks.OAK_LOG_BOULDER.get()) {
+                    return registration.blockAccessor().from(blockAccessor).blockState(Blocks.OAK_LOG.defaultBlockState()).build();
                 }
             } else if (accessor instanceof EntityAccessor entityAccessor) {
                 if (entityAccessor.getEntity() instanceof TreasureBagItemEntity entity && !entity.isOwner(accessor.getPlayer())) {

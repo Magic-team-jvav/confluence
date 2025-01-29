dcca304f124b255c939b85fc2d0bb5092ed173e2
westernat <westernat233@163.com>
2025-01-29 16:52:43 +0800

    flag可以使用字符串解析

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/worldgen/feature/SecretFlagFeature.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/worldgen/feature/SecretFlagFeature.java
@@ -5,13 +5,15 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import org.confluence.mod.Confluence;
 import org.confluence.mod.mixed.IMinecraftServer;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
 
 public class SecretFlagFeature extends Feature<SecretFlagFeature.Config> {
     public SecretFlagFeature(Codec<Config> codec) {
@@ -20,12 +22,10 @@ public class SecretFlagFeature extends Feature<SecretFlagFeature.Config> {
 
     @Override
     public boolean place(@NotNull FeaturePlaceContext<Config> context) {
-        MinecraftServer server = context.level().getServer();
-        if (server != null && ((IMinecraftServer) server).confluence$matchesSecretFlag(context.config().flag)) {
+        if (((IMinecraftServer) context.level().getLevel().getServer()).confluence$matchesSecretFlag(context.config().flag)) {
             Either<ResourceLocation, ConfiguredFeature<?, ?>> subFeature = context.config().subFeature;
             return subFeature.map(
-                    id -> context.level().registryAccess().registryOrThrow(Registries.CONFIGURED_FEATURE)
-                            .getHolder(id).orElseThrow().value()
+                    id -> context.level().registryAccess().registryOrThrow(Registries.CONFIGURED_FEATURE).getHolder(id).orElseThrow().value()
                             .place(context.level(), context.chunkGenerator(), context.random(), context.origin()),
                     cf -> cf.place(context.level(), context.chunkGenerator(), context.random(), context.origin())
             );
@@ -36,7 +36,22 @@ public class SecretFlagFeature extends Feature<SecretFlagFeature.Config> {
     public record Config(Either<ResourceLocation, ConfiguredFeature<?, ?>> subFeature, long flag) implements FeatureConfiguration {
         public static final Codec<Config> CODEC = RecordCodecBuilder.create(instance -> instance.group(
                 Codec.either(ResourceLocation.CODEC, ConfiguredFeature.DIRECT_CODEC).fieldOf("sub_feature").forGetter(Config::subFeature),
-                Codec.LONG.fieldOf("flag").forGetter(Config::flag)
-        ).apply(instance, Config::new));
+                Codec.either(Codec.LONG, Codec.STRING).fieldOf("flag").forGetter(config -> Either.left(config.flag))
+        ).apply(instance, (sf, num) -> new Config(sf, num.map(Function.identity(), str -> {
+            long ret = 0;
+            if (str.startsWith("&")) {
+                try {
+                    ret = Long.parseLong(str.substring(1), 2);
+                } catch (NumberFormatException ignored) {}
+            } else {
+                try {
+                    ret = Long.parseLong(str);
+                } catch (NumberFormatException ignored) {}
+            }
+            if (ret == 0) {
+                Confluence.LOGGER.error("Can not decode '{}'!", str);
+            }
+            return ret;
+        }))));
     }
 }

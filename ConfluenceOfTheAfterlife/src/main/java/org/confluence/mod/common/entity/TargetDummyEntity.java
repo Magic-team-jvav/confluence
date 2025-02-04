ed8af4a6fa2ff82d8eb3e0080eca5036b6f14544
sihuai2412 <sihuai2022@163.com>
2025-02-04 15:54:49 +0800

    小改

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/entity/TargetDummyEntity.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/entity/TargetDummyEntity.java
@@ -1,10 +1,8 @@
 package org.confluence.mod.common.entity;
 
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
@@ -13,14 +11,12 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import org.confluence.mod.common.particle.DamageIndicatorOptions;
-import org.confluence.mod.mixed.IDamageSource;
-import org.confluence.terra_curio.common.init.TCAttributes;
+import org.confluence.mod.common.init.item.ModItems;
+import org.confluence.mod.util.ModUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class TargetDummyEntity extends LivingEntity {
@@ -34,30 +30,10 @@ public class TargetDummyEntity extends LivingEntity {
         if (source.getEntity() instanceof Player player){
             if (player.getItemInHand(InteractionHand.MAIN_HAND).getItem() instanceof PickaxeItem){
                 this.remove(RemovalReason.DISCARDED);
+                ModUtils.createItemEntity(ModItems.TARGET_DUMMY.get().getDefaultInstance(), position(), player.level(), 0);
                 return true;
             }
         }
-
-        float damage = amount;
-        boolean crit = false;
-        if (!TCAttributes.hasCustomAttribute(TCAttributes.CRIT_CHANCE) && source.getEntity() instanceof Player player) {
-            double chance = player.getAttributeValue(TCAttributes.CRIT_CHANCE);
-            if (this.level().random.nextFloat() < chance) {
-                damage *= 2;
-                player.crit(this);
-                crit = true;
-            }
-        }
-        if (source.getDirectEntity() instanceof AbstractArrow arrow) {
-            crit |= arrow.isCritArrow();
-        }
-        crit |= ((IDamageSource) source).confluence$isCritical();
-        float roundedAmount = Math.round(damage * 10) / 10f;
-        int intAmount = (int) roundedAmount;
-        String text = roundedAmount % 1 == 0 ? String.valueOf(intAmount) : String.valueOf(roundedAmount);
-        Component damageComponent = Component.literal(text)
-                .withStyle(crit ? ChatFormatting.DARK_RED : ChatFormatting.GOLD, ChatFormatting.BOLD);
-
 
         this.level().playSound(null, this.getX(), this.getY(), this.getZ(), SoundEvents.ARMOR_STAND_BREAK, this.getSoundSource(), 1.0F, 1.0F); // playBrokenSound
         if (this.level() instanceof ServerLevel sl) {
@@ -71,14 +47,8 @@ public class TargetDummyEntity extends LivingEntity {
                     this.getBbHeight() / 4.0F,
                     this.getBbWidth() / 4.0F, 0.05
             );
-            sl.sendParticles(
-                    new DamageIndicatorOptions(damageComponent, crit),
-                    this.position().x,
-                    this.getBoundingBoxForCulling().maxY,
-                    this.position().z, 1, 0.1, 0.1, 0.1, 0);
-
         }  // showParticles
-        return super.hurt(source, amount <= 1000000F ? 0 : amount);
+        return super.hurt(source, amount);
     }
 
     @Override
@@ -98,6 +68,14 @@ public class TargetDummyEntity extends LivingEntity {
     @Override
     public HumanoidArm getMainArm() {
         return HumanoidArm.RIGHT;
+    }
+
+    @Override
+    public void tick() {
+        if (getHealth() != getMaxHealth()){
+            heal(getMaxHealth());
+        }
+        super.tick();
     }
 
     public static AttributeSupplier.@NotNull Builder createAttributes() {

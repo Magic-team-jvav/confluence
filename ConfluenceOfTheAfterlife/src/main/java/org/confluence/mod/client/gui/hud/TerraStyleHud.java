491da80c9f6f8c8fbfdff17adc6bcf1af12e1629
Lazy-Pillow-Minecraft <wsc1122330@163.com>
2025-02-05 15:30:42 +0800

    覆盖风格血条

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/client/gui/hud/TerraStyleHud.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/client/gui/hud/TerraStyleHud.java
@@ -1,7 +1,7 @@
 package org.confluence.mod.client.gui.hud;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import it.unimi.dsi.fastutil.floats.FloatArrayList;
-import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
@@ -9,6 +9,7 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -24,12 +25,17 @@ import org.confluence.mod.util.ClientUtils;
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Locale;
+import java.util.Random;
 
 @ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
 public class TerraStyleHud implements LayeredDraw.Layer {
-    private static final ResourceLocation ICON = Confluence.asResource("textures/gui/hud/icon.png");
-    private static final int SIZE = 128;
+    private static final ResourceLocation LEGACY_TEXTURE = Confluence.asResource("textures/gui/hud/icon.png");
+    private static final ResourceLocation OVERLAY_TEXTURE = Confluence.asResource("textures/gui/hud/overlay.png");
+    private static final int LEGACY_SIZE = 128;
+    private static final int OVERLAY_SIZE = 128;
+    private static final int[] HEALTH = new int[]{0xab311e, 0x5d11ba, 0x436dd0, 0x37c438, 0xeed536};
+    private static final int[] HEALTH_LOW = new int[]{0xab1f5d, 0x9d44ac, 0x5d11ba, 0x266c71, 0xf7b60b};
+    private static final int[] HEALTH_HIGH = new int[]{0xffb5b5, 0xd6e7eb, 0xc5d4f8, 0xd6eead, 0xeff4ce};
 
     @Override
     public void render(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
@@ -45,7 +51,78 @@ public class TerraStyleHud implements LayeredDraw.Layer {
         minecraft.getProfiler().pop();
     }
 
-    public enum Armor implements TranslatableEnum {
+    private static void drawString(GuiGraphics guiGraphics, Font font, @Nullable String text, float x, float y, int color) {
+        guiGraphics.drawString(font, text, x + 1, y, 0x000000, false);
+        guiGraphics.drawString(font, text, x - 1, y, 0x000000, false);
+        guiGraphics.drawString(font, text, x, y + 1, 0x000000, false);
+        guiGraphics.drawString(font, text, x, y - 1, 0x000000, false);
+        guiGraphics.drawString(font, text, x, y, color, false);
+    }
+
+    private static void drawColor(GuiGraphics guiGraphics, int x, int y, int iconX, int iconY, ResourceLocation icon, int color, int colorHigh, int colorLow) {
+        float red = ((color >> 16) & 0xFF) / 255.0F;
+        float green = ((color >> 8) & 0xFF) / 255.0F;
+        float blue = (color & 0xFF) / 255.0F;
+        float redHigh = ((colorHigh >> 16) & 0xFF) / 255.0F;
+        float greenHigh = ((colorHigh >> 8) & 0xFF) / 255.0F;
+        float blueHigh = (colorHigh & 0xFF) / 255.0F;
+        float redLow = ((colorLow >> 16) & 0xFF) / 255.0F;
+        float greenLow = ((colorLow >> 8) & 0xFF) / 255.0F;
+        float blueLow = (colorLow & 0xFF) / 255.0F;
+        RenderSystem.setShaderColor(red, green, blue, 1.0F);
+        guiGraphics.blit(icon, x, y, iconX, iconY, 9, 9, OVERLAY_SIZE, OVERLAY_SIZE);
+        RenderSystem.setShaderColor(redLow, greenLow, blueLow, 1.0F);
+        guiGraphics.blit(icon, x, y, iconX + 20, iconY, 9, 9, OVERLAY_SIZE, OVERLAY_SIZE);
+        RenderSystem.setShaderColor(redHigh, greenHigh, blueHigh, 1.0F);
+        guiGraphics.blit(icon, x, y, iconX + 40, iconY, 9, 9, OVERLAY_SIZE, OVERLAY_SIZE);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    private static int colorHigh(int color) {
+        return (color / 255 * 55 + 200);
+    }
+
+    private static int colorLow(int color, RandomSource random) {
+        int colorT = (color - 60 + random.nextInt(121));
+        if (colorT < 0) {colorT = 0;}
+        if (colorT > 255) {colorT = 255;}
+        return colorT;
+    }
+
+    private static void draw(int x, int y, GuiGraphics guiGraphics, RandomSource random, int count) {
+        int R;
+        int G;
+        int B;
+        do {
+            R = random.nextInt(256);
+            G = random.nextInt(256);
+            B = random.nextInt(256) + 255 - R - G;
+        } while (B > 255 || B < 0);
+        int color = (R << 16) | (G << 8) | B;
+        int colorHigh =  (colorHigh(R) << 16) | (colorHigh(G) << 8) | colorHigh(B);
+        int colorLow =  (colorLow(R, random) << 16) | (colorLow(G, random) << 8) | colorLow(B, random);
+        int countT = count / 2;
+        int xT = x - 8;
+        for (int i = 0; i < countT; i++) {
+            xT = x + i * 8;
+            drawColor(guiGraphics, xT, y, 0, 0, OVERLAY_TEXTURE, color, colorHigh, colorLow);
+        } if (count - countT * 2 == 1) {
+            drawColor(guiGraphics, xT + 8, y, 10, 0, OVERLAY_TEXTURE, color, colorHigh, colorLow);
+        }
+    }
+
+    private static void draw(int x, int y, GuiGraphics guiGraphics, int count, int color, int colorHigh, int colorLow) {
+        int countT = count / 2;
+        int xT = x - 8;
+        for (int i = 0; i < countT; i++) {
+            xT = x + i * 8;
+            drawColor(guiGraphics, xT, y, 0, 0, OVERLAY_TEXTURE, color, colorHigh, colorLow);
+        } if (count - countT * 2 == 1) {
+            drawColor(guiGraphics, xT + 8, y, 10, 0, OVERLAY_TEXTURE, color, colorHigh, colorLow);
+        }
+    }
+
+    public enum Armor {
         LEGACY_HORIZONTAL {
             @Override
             public void render(GuiGraphics guiGraphics, Minecraft minecraft) {
@@ -64,14 +141,6 @@ public class TerraStyleHud implements LayeredDraw.Layer {
                 draw(guiGraphics, minecraft, 3);
             }
         };
-
-        private void drawString(GuiGraphics guiGraphics, Font font, @Nullable String text, float x, float y, int color) {
-            guiGraphics.drawString(font, text, x + 1, y, 0x000000, false);
-            guiGraphics.drawString(font, text, x - 1, y, 0x000000, false);
-            guiGraphics.drawString(font, text, x, y + 1, 0x000000, false);
-            guiGraphics.drawString(font, text, x, y - 1, 0x000000, false);
-            guiGraphics.drawString(font, text, x, y, color, false);
-        }
 
         public void draw(GuiGraphics guiGraphics, Minecraft minecraft, int type) {
             int armorNum = 0;
@@ -110,7 +179,7 @@ public class TerraStyleHud implements LayeredDraw.Layer {
             widthOffset = (widthOffset > 0) ? widthOffset : 0;
             guiGraphics.pose().pushPose();
             guiGraphics.pose().translate(-widthOffset, 0.0F, 0.0F);
-            guiGraphics.blit(ICON, widthArmor, heightArmor, 0, 51, 23, 25, SIZE, SIZE);
+            guiGraphics.blit(LEGACY_TEXTURE, widthArmor, heightArmor, 0, 51, 23, 25, LEGACY_SIZE, LEGACY_SIZE);
             guiGraphics.pose().popPose();
             if (armorToughnessNum == 0) {
                 drawString(guiGraphics, minecraft.font, armor, widthArmor + 11.5F - (textWidthArmor / 2.0F) - widthOffset, heightArmor + 12.5F - (textHeight / 2.0F), colorArmor);
@@ -132,7 +201,7 @@ public class TerraStyleHud implements LayeredDraw.Layer {
             }
         }
 
-        public static String str(String text, int count) {
+        private static String str(String text, int count) {
             String str = "";
             for (int i = 0; i < count; i++) {
                 str += text;
@@ -141,33 +210,29 @@ public class TerraStyleHud implements LayeredDraw.Layer {
         }
 
         public abstract void render(GuiGraphics guiGraphics, Minecraft minecraft);
-
-        @Override
-        public Component getTranslatedName() {
-            return Component.translatable("confluence.configuration.armorStyle." + name().toLowerCase(Locale.ROOT));
-        }
     }
 
     public enum Mana implements TranslatableEnum {
         LEGACY {
             @Override
             public void render(GuiGraphics guiGraphics, Minecraft minecraft) {
-                int widthMana = guiGraphics.guiWidth() - 23;
+                int widthMana = guiGraphics.guiWidth() - 21;
+                int heightMana = 4;
                 int currentMana = ClientPacketHandler.getCurrentMana();
                 int maxManaCount = ClientPacketHandler.getMaxMana() / 20;
                 int currentManaToBlit;
                 float ts;
                 for (int i = 0; i < maxManaCount; i++) {
                     currentManaToBlit = currentMana - (i + 1) * 20;
-                    guiGraphics.blit(ICON, widthMana, 10 + i * 12, 0, 34, 17, 16, SIZE, SIZE);
+                    guiGraphics.blit(LEGACY_TEXTURE, widthMana, heightMana + i * 12, 0, 34, 17, 16, LEGACY_SIZE, LEGACY_SIZE);
                     if (currentManaToBlit >= 0) {
-                        guiGraphics.blit(ICON, widthMana + 2, 10 + i * 12, 18, 34, 13, 16, SIZE, SIZE);
+                        guiGraphics.blit(LEGACY_TEXTURE, widthMana + 2, heightMana + i * 12, 18, 34, 13, 16, LEGACY_SIZE, LEGACY_SIZE);
                     } else if (currentManaToBlit + 20 >= 0) {
                         ts = ((float) (currentManaToBlit + 20)) / 20.0F;
                         guiGraphics.pose().pushPose();
-                        guiGraphics.pose().translate(widthMana + 2 + 6.5F * (1 - ts), 10 + i * 12 + 8.5F * (1 - ts), 0.0F);
+                        guiGraphics.pose().translate(widthMana + 2 + 6.5F * (1 - ts), heightMana + i * 12 + 8.5F * (1 - ts), 0.0F);
                         guiGraphics.pose().scale(ts, ts, 1.0F);
-                        guiGraphics.blit(ICON, 0, 0, 18, 34, 13, 16, SIZE, SIZE);
+                        guiGraphics.blit(LEGACY_TEXTURE, 0, 0, 18, 34, 13, 16, LEGACY_SIZE, LEGACY_SIZE);
                         guiGraphics.pose().popPose();
                     }
                 }
@@ -186,7 +251,8 @@ public class TerraStyleHud implements LayeredDraw.Layer {
         LEGACY {
             @Override
             public void render(GuiGraphics guiGraphics, Minecraft minecraft) {
-                int widthHealth = guiGraphics.guiWidth() - 130;
+                int widthHealth = guiGraphics.guiWidth() - 128;
+                int heightHealth = 4;
                 float maxHealth = 0.0F;
                 float currentHealth = 0.0F;
                 int heartBuff = 0;
@@ -210,7 +276,7 @@ public class TerraStyleHud implements LayeredDraw.Layer {
                 int countHeart = (int) ((maxHealth - (float) lifeFruitHealth) / 4);
                 int countHeartAbsorptionHealth = (int) (absorptionHealth / 4.0F) + ((((int) absorptionHealth) % 4 == 0) ? 0 : 1);
                 int yLine = 4 + (int) (6.0F / (float) ((countHeart - 1) / 20 + (countHeartAbsorptionHealth - 1) / 20 + 1));
-                int heightToAbsorption = blit(heartBuff, maxHealth, currentHealth, lifeFruitHealth, guiGraphics, widthHealth, 10, 0, yLine);
+                int heightToAbsorption = blit(heartBuff, maxHealth, currentHealth, lifeFruitHealth, guiGraphics, widthHealth, heightHealth, 0, yLine);
                 blit(0, countHeartAbsorptionHealth * 4, absorptionHealth, 0, guiGraphics, widthHealth, heightToAbsorption, 1, yLine);
             }
 
@@ -219,13 +285,13 @@ public class TerraStyleHud implements LayeredDraw.Layer {
                 heartBuff = (typeHeart == 0) ? heartBuff : 0;
                 int heightUV = (typeHeart * 17);
                 if (num == heartNum) {
-                    guiGraphics.blit(ICON, x, y, ((type == 1) ? 54 : 44) + heartBuff, heightUV, 9, 16, SIZE, SIZE);
+                    guiGraphics.blit(LEGACY_TEXTURE, x, y, ((type == 1) ? 54 : 44) + heartBuff, heightUV, 9, 16, LEGACY_SIZE, LEGACY_SIZE);
                 } else if (num > 0.0F) {
                     float ts = num / heartNum;
                     guiGraphics.pose().pushPose();
                     guiGraphics.pose().translate(x + 4.5F * (1.0F - ts), y + 9.0F * (1.0F - ts), 0.0F);
                     guiGraphics.pose().scale(ts, ts, 1.0F);
-                    guiGraphics.blit(ICON, 0, 0, ((type == 1) ? 54 : 44) + heartBuff, heightUV, 9, 16, SIZE, SIZE);
+                    guiGraphics.blit(LEGACY_TEXTURE, 0, 0, ((type == 1) ? 54 : 44) + heartBuff, heightUV, 9, 16, LEGACY_SIZE, LEGACY_SIZE);
                     guiGraphics.pose().popPose();
                 }
             }
@@ -246,18 +312,18 @@ public class TerraStyleHud implements LayeredDraw.Layer {
                     blitX = countToBlit * 10 - countLine * 100;
                     blitXFirst = (countHeart / 10 == 0) ? 100 - (countHeart - (countHeart / 10) * 10) * 10 : 0;
                     if (countToBlit % 10 == 0) {
-                        guiGraphics.blit(ICON, width + blitX - 1 + blitXFirst, height + blitY, 0, heightUV, 1, 16, SIZE, SIZE);
+                        guiGraphics.blit(LEGACY_TEXTURE, width + blitX - 1 + blitXFirst, height + blitY, 0, heightUV, 1, 16, LEGACY_SIZE, LEGACY_SIZE);
                         if (countToBlit + 1 == countHeart) {
-                            guiGraphics.blit(ICON, width + blitX - 3 + blitXFirst, height + blitY, 13, heightUV, 17, 16, SIZE, SIZE);
+                            guiGraphics.blit(LEGACY_TEXTURE, width + blitX - 3 + blitXFirst, height + blitY, 13, heightUV, 17, 16, LEGACY_SIZE, LEGACY_SIZE);
                         } else {
-                            guiGraphics.blit(ICON, width + blitX + blitXFirst, height + blitY, 2, heightUV, 10, 16, SIZE, SIZE);
+                            guiGraphics.blit(LEGACY_TEXTURE, width + blitX + blitXFirst, height + blitY, 2, heightUV, 10, 16, LEGACY_SIZE, LEGACY_SIZE);
                         }
                     } else if (countToBlit + 1 == countHeart) {
-                        guiGraphics.blit(ICON, width + blitX - 3 + blitXFirst, height + blitY, 13, heightUV, 17, 16, SIZE, SIZE);
+                        guiGraphics.blit(LEGACY_TEXTURE, width + blitX - 3 + blitXFirst, height + blitY, 13, heightUV, 17, 16, LEGACY_SIZE, LEGACY_SIZE);
                     } else if ((countToBlit + 1) % 10 == 0) {
-                        guiGraphics.blit(ICON, width + blitX + blitXFirst, height + blitY, 31, heightUV, 12, 16, SIZE, SIZE);
+                        guiGraphics.blit(LEGACY_TEXTURE, width + blitX + blitXFirst, height + blitY, 31, heightUV, 12, 16, LEGACY_SIZE, LEGACY_SIZE);
                     } else {
-                        guiGraphics.blit(ICON, width + blitX + blitXFirst, height + blitY, 2, heightUV, 10, 16, SIZE, SIZE);
+                        guiGraphics.blit(LEGACY_TEXTURE, width + blitX + blitXFirst, height + blitY, 2, heightUV, 10, 16, LEGACY_SIZE, LEGACY_SIZE);
                     }
                     type = (countToBlit < lifeFruitHealth) ? 1 : 0;
                     blitHeart(guiGraphics, type, typeHeart, heartNumList.getFloat(countToBlit), width + blitX + blitXFirst + 1, height + blitY, heartBuff);
@@ -283,6 +349,46 @@ public class TerraStyleHud implements LayeredDraw.Layer {
                     }
                 }
                 return heartNumList;
+            }
+        },
+        OVERLAY {
+            @Override
+            public void render(GuiGraphics guiGraphics, Minecraft minecraft) {
+                float maxHealth = 0.0F;
+                float currentHealth = 0.0F;
+                Player player = minecraft.player;
+                if (player != null) {
+                    maxHealth = player.getMaxHealth();
+                    currentHealth = player.getHealth();
+                }
+                int widthHealth = guiGraphics.guiWidth() / 2 - 91;
+                int heightHealth = guiGraphics.guiHeight() - 39;
+                RandomSource random = RandomSource.create(114514);
+                int backCount = (int) (maxHealth / 2);
+                int heartCount = (int) (currentHealth);
+                if (maxHealth / 2 > (float) backCount) {backCount++;}
+                if (currentHealth > (float) heartCount) {heartCount++;}
+                for (int i = 0; i < backCount && i < 10; i++) {
+                    guiGraphics.blit(OVERLAY_TEXTURE, widthHealth + i * 8, heightHealth, 60, 0, 9, 9, OVERLAY_SIZE, OVERLAY_SIZE);
+                }
+                int lineCount = heartCount / 20;
+                int drawCount;
+                int lineCountDraw = lineCount;
+                for (int i = 0; i <= lineCount; i++) {
+                    drawCount = (i == lineCount) ? (heartCount % 20) : 20;
+                    if (i < HEALTH.length) {
+                        draw(widthHealth, heightHealth, guiGraphics, drawCount, HEALTH[i], HEALTH_HIGH[i], HEALTH_LOW[i]);
+                    } else {
+                        draw(widthHealth, heightHealth, guiGraphics, random, drawCount);
+                    }
+                    if (drawCount != 20 && drawCount != 0) {
+                        lineCountDraw = lineCount + 1;
+                    }
+                }
+                String drawString = Integer.toString(lineCountDraw);
+                if (lineCountDraw > 1) {
+                    drawString(guiGraphics, minecraft.font, drawString, widthHealth - 3 - minecraft.font.width(Component.literal(drawString)), heightHealth + 1, 0xFFFFFF);
+                }
             }
         };
 

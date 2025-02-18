ef2ad4587d5bafc77d48abbe1b03fbb92bc7e902
voila <voila1106@gmail.com>
2025-02-18 23:39:20 +0800

    改假人资源位置

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/client/model/entity/TargetDummyModel.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/client/model/entity/TargetDummyModel.java
@@ -3,14 +3,13 @@ package org.confluence.mod.client.model.entity;
 import net.minecraft.resources.ResourceLocation;
 import org.confluence.mod.Confluence;
 import org.confluence.mod.common.entity.TargetDummyEntity;
-import org.confluence.terraentity.TerraEntity;
 import software.bernie.geckolib.model.GeoModel;
 
 @SuppressWarnings("removal")
 public class TargetDummyModel extends GeoModel<TargetDummyEntity> {
-    private static final ResourceLocation MODEL = TerraEntity.space("geo/entity/target_dummy.geo.json");
+    private static final ResourceLocation MODEL = Confluence.asResource("geo/entity/target_dummy.geo.json");
     private static final ResourceLocation TEXTURE = Confluence.asResource("textures/entity/target_dummy.png");
-    private static final ResourceLocation ANIMATION = TerraEntity.space("animations/entity/target_dummy.animation.json");
+    private static final ResourceLocation ANIMATION = Confluence.asResource("animations/entity/target_dummy.animation.json");
 
     @Override
     public ResourceLocation getModelResource(TargetDummyEntity animatable) {

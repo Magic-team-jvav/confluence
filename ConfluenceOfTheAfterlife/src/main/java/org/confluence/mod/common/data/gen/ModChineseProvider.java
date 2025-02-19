40245d8c0a0ea38e526d2017ccc5c0da2842f2f4
westernat <westernat233@163.com>
2025-02-19 17:28:29 +0800

    修rundata

--- ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/data/gen/ModChineseProvider.java
+++ ConfluenceOfTheAfterlife/src/main/java/org/confluence/mod/common/data/gen/ModChineseProvider.java
@@ -1982,7 +1982,7 @@ public class ModChineseProvider extends LanguageProvider {
         add(ManaWeapons.BETSYS_WRATH.get(), "双足翼龙怒气");
          */
         add(ManaWeaponItems.BEE_GUN.get(), "蜜蜂枪");
-        add(ManaWeaponItems.BEE_GUN.get(), "太空枪");
+        add(ManaWeaponItems.SPACE_GUN.get(), "太空枪");
 
         // 弓
         add(BowItems.WOODEN_SHORT_BOW.get(), "木短弓");

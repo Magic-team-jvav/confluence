2e604045d2dd11a7f860e55ab436cd44a0da06ae
westernat <westernat233@163.com>
2025-03-04 18:51:39 +0800

    修复玩家使用水上漂时，挖掘方块不同步的问题

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/integration/terra_curio/TCUtilsMixin.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/integration/terra_curio/TCUtilsMixin.java
@@ -3,14 +3,14 @@ package org.confluence.mod.mixin.integration.terra_curio;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.material.FluidState;
 import org.confluence.mod.common.init.ModEffects;
-import org.confluence.terra_curio.client.handler.TCClientPacketHandler;
+import org.confluence.terra_curio.util.TCUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = TCClientPacketHandler.class, remap = false)
-public abstract class TCClientPacketHandlerMixin {
+@Mixin(value = TCUtils.class, remap = false)
+public abstract class TCUtilsMixin {
     @Inject(method = "isFluidWalkable", at = @At("RETURN"), cancellable = true)
     private static void waterWalkingEffect(LivingEntity living, FluidState fluidState, CallbackInfoReturnable<Boolean> cir) {
         if (!cir.getReturnValue() && living.hasEffect(ModEffects.WATER_WALKING)) {

1e6dc1887f1580b2e241e60245b6fab96a26d635
westernat <westernat233@163.com>
2025-03-11 17:14:10 +0800

    与钠：动态光源的兼容

---
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/integration/terra_curio/TCClientPacketHandlerMixin.java
@@ -0,0 +1,18 @@
+package org.confluence.mod.mixin.integration.terra_curio;
+
+import net.minecraft.world.entity.Entity;
+import org.confluence.mod.util.ClientUtils;
+import org.confluence.terra_curio.client.handler.TCClientPacketHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = TCClientPacketHandler.class, remap = false)
+public abstract class TCClientPacketHandlerMixin {
+    @Inject(method = "getLuminance", at = @At("RETURN"), cancellable = true)
+    private static void extra(Entity entity, CallbackInfoReturnable<Integer> cir) {
+        int luminance = ClientUtils.getLuminance(entity, cir.getReturnValue());
+        if (luminance != 0) cir.setReturnValue(luminance);
+    }
+}

0def2fbfcc548bdf53bbd8d3ae902c8cdd8cce27
the_xu <448537509@qq.com>
2025-03-13 20:09:29 +0800

    魔光剑单体只能触发一次

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/entity/projectile/sword/LightBaneProjectile.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/entity/projectile/sword/LightBaneProjectile.java
@@ -15,10 +15,13 @@ import org.confluence.mod.common.init.ModParticleTypes;
 import org.joml.Vector3f;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LightBaneProjectile extends SwordProjectile {
 
     public Vec3 direction;
+    List<Entity> hits = new ArrayList<>();
     public static final EntityDataAccessor<Vector3f> DATA_DIRECTION = SynchedEntityData.defineId(LightBaneProjectile.class, EntityDataSerializers.VECTOR3);
 
     public LightBaneProjectile(EntityType<LightBaneProjectile> entityType, Level pLevel) {
@@ -43,8 +46,14 @@ public class LightBaneProjectile extends SwordProjectile {
     }
 
     @Override
+    protected boolean canHitEntity(Entity target) {
+        return  !hits.contains(target) && super.canHitEntity(target);
+    }
+
+    @Override
     protected boolean doHurt(Entity living) {
         if(super.doHurt(living)){
+            hits.add(living);
             ((ServerLevel) level()).sendParticles(ModParticleTypes.LIGHT_BANE.get(),getX(),getY(),getZ(),1,0,0,0,0);
             return true;
         }

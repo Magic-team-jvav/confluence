7a4793d285dabcf73ee1e11bc50ee03a2e9c2e32
westernat <westernat233@163.com>
2025-03-22 18:17:05 +0800

    修复

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/worldgen/structure/SimpleTemplatePiece.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/worldgen/structure/SimpleTemplatePiece.java
@@ -1,26 +1,20 @@
 package org.confluence.mod.common.worldgen.structure;
 
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.minecraft.commands.arguments.blocks.BlockStateParser;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.StructureManager;
 import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.Rotation;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.StructureMode;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.TemplateStructurePiece;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceSerializationContext;
 import net.minecraft.world.level.levelgen.structure.templatesystem.BlockIgnoreProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
-import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 import org.confluence.mod.Confluence;
 import org.confluence.mod.common.init.ModStructures;
@@ -28,22 +22,15 @@ import org.confluence.mod.common.init.ModStructures;
 public class SimpleTemplatePiece extends TemplateStructurePiece {
     public SimpleTemplatePiece(StructureTemplateManager structureTemplateManager, String name, BlockPos startPos, boolean overwrite, boolean ignoreEntities, Rotation rotation) {
         super(ModStructures.SIMPLE_TEMPLATE_PIECE.get(), 0, structureTemplateManager, Confluence.asResource("simple_template/" + name), name, makeSettings(overwrite, ignoreEntities, rotation), startPos);
-        makeBigBox();
     }
 
     public SimpleTemplatePiece(StructureTemplateManager structureTemplateManager, CompoundTag tag) {
         super(ModStructures.SIMPLE_TEMPLATE_PIECE.get(), tag, structureTemplateManager, location -> makeSettings(tag.getBoolean("OW"), tag.getBoolean("IE"), Rotation.valueOf(tag.getString("Rot"))));
-        makeBigBox();
     }
 
-    private void makeBigBox() {
-        int i = templatePosition.getX() / 16 * 16;
-        int j = templatePosition.getZ() / 16 * 16;
-        this.boundingBox = new BoundingBox(
-                i - 16, templatePosition.getY() - 12, j - 16,
-                i + 31, templatePosition.getY() + 11, j + 31
-        );
-        placeSettings.setBoundingBox(boundingBox);
+    @Override
+    protected ResourceLocation makeTemplateLocation() {
+        return Confluence.asResource("simple_template/" + templateName);
     }
 
     private static StructurePlaceSettings makeSettings(boolean overwrite, boolean ignoreEntities, Rotation rotation) {
@@ -61,39 +48,8 @@ public class SimpleTemplatePiece extends TemplateStructurePiece {
 
     @Override
     public void postProcess(WorldGenLevel level, StructureManager structureManager, ChunkGenerator generator, RandomSource random, BoundingBox box, ChunkPos chunkPos, BlockPos pos) {
-        if (this.template.placeInWorld(level, this.templatePosition, pos, this.placeSettings, random, 2)) {
-            for (StructureTemplate.StructureBlockInfo structuretemplate$structureblockinfo : this.template
-                    .filterBlocks(this.templatePosition, this.placeSettings, Blocks.STRUCTURE_BLOCK)) {
-                if (structuretemplate$structureblockinfo.nbt() != null) {
-                    StructureMode structuremode = StructureMode.valueOf(structuretemplate$structureblockinfo.nbt().getString("mode"));
-                    if (structuremode == StructureMode.DATA) {
-                        this.handleDataMarker(
-                                structuretemplate$structureblockinfo.nbt().getString("metadata"),
-                                structuretemplate$structureblockinfo.pos(),
-                                level,
-                                random,
-                                box
-                        );
-                    }
-                }
-            }
-
-            for (StructureTemplate.StructureBlockInfo structuretemplate$structureblockinfo1 : this.template
-                    .filterBlocks(this.templatePosition, this.placeSettings, Blocks.JIGSAW)) {
-                if (structuretemplate$structureblockinfo1.nbt() != null) {
-                    String s = structuretemplate$structureblockinfo1.nbt().getString("final_state");
-                    BlockState blockstate = Blocks.AIR.defaultBlockState();
-
-                    try {
-                        blockstate = BlockStateParser.parseForBlock(level.holderLookup(Registries.BLOCK), s, true).blockState();
-                    } catch (CommandSyntaxException commandsyntaxexception) {
-                        Confluence.LOGGER.error("Error while parsing blockstate {} in jigsaw block @ {}", s, structuretemplate$structureblockinfo1.pos());
-                    }
-
-                    level.setBlock(structuretemplate$structureblockinfo1.pos(), blockstate, 3);
-                }
-            }
-        }
+        //box.encapsulate(this.template.getBoundingBox(this.placeSettings, this.templatePosition));
+        super.postProcess(level, structureManager, generator, random, box, chunkPos, pos);
     }
 
     @Override

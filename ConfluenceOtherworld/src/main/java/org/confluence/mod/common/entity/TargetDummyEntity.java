5ae77f26250d7061a3ea1c1373fc52f14348a4f4
westernat <westernat233@163.com>
2025-03-23 20:48:01 +0800

    调整魔法伤害，跳过假人交互

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/entity/TargetDummyEntity.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/entity/TargetDummyEntity.java
@@ -8,11 +8,14 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
@@ -99,7 +102,7 @@ public class TargetDummyEntity extends Mob {
             }
         }
 
-        return InteractionResult.SUCCESS;
+        return InteractionResult.PASS;
     }
 
     private void swapItem(Player player, EquipmentSlot slot, ItemStack armor, InteractionHand hand) {

0e962179839e7a488aae35f9086d710c39afbc56
Ironnoob73 <67966070+Ironnoob73@users.noreply.github.com>
2025-03-31 23:18:38 +0800

    Zombie Name
    使用检测装备的方式更改雨衣僵尸、冰冻僵尸、不死矿工的名称显示。

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/event/game/entity/LivingEntityEvents.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/event/game/entity/LivingEntityEvents.java
@@ -388,12 +388,12 @@ public final class LivingEntityEvents {
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.CHEST, (pink ? ArmorItems.PINK_SNOW_SUITS : ArmorItems.SNOW_SUITS).get(), 0.003F);
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.LEGS, (pink ? ArmorItems.PINK_INSULATED_PANTS : ArmorItems.INSULATED_PANTS).get(), 0.003F);
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.FEET, (pink ? ArmorItems.PINK_INSULATED_SHOES : ArmorItems.INSULATED_SHOES).get(), 0.003F);
-                mob.setCustomName(Component.translatable("entity.confluence.frozen_zombie"));
+                //mob.setCustomName(Component.translatable("entity.confluence.frozen_zombie"));
                 event.setCanceled(true);
             } else if (level.isRainingAt(blockPos)) {
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.HEAD, ArmorItems.RAIN_CAP.get(), 0.003F);
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.CHEST, ArmorItems.RAINCOAT.get(), 0.003F);
-                mob.setCustomName(Component.translatable("entity.confluence.raincoat_zombie"));
+                //mob.setCustomName(Component.translatable("entity.confluence.raincoat_zombie"));
                 event.setCanceled(true);
             }
         } else if (mob.getType() == EntityType.SKELETON) {
@@ -403,7 +403,7 @@ public final class LivingEntityEvents {
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.LEGS, ArmorItems.MINING_LEGGINGS.get(), 1.0F);
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.FEET, ArmorItems.MINING_BOOTS.get(), 1.0F);
                 ModUtils.setItemAndDropChance(mob, difficulty, EquipmentSlot.MAINHAND, PickaxeItems.BONE_PICKAXE.get(), 0.25F);
-                mob.setCustomName(Component.translatable("entity.confluence.undead_miner"));
+                //mob.setCustomName(Component.translatable("entity.confluence.undead_miner"));
                 event.setCanceled(true);
             }
         }
--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/entity/EntityMixin.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/entity/EntityMixin.java
@@ -1,13 +1,23 @@
 package org.confluence.mod.mixin.entity;
 
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentContents;
+import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.monster.Skeleton;
+import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.common.NeoForge;
@@ -18,6 +28,7 @@ import org.confluence.mod.common.data.saved.ConfluenceData;
 import org.confluence.mod.common.data.saved.GamePhase;
 import org.confluence.mod.common.init.ModFluids;
 import org.confluence.mod.common.init.ModSoundEvents;
+import org.confluence.mod.common.init.item.ArmorItems;
 import org.confluence.mod.mixed.IEntity;
 import org.confluence.terra_curio.mixed.SelfGetter;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,6 +37,9 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 import static org.confluence.mod.api.event.ShimmerEntityTransmutationEvent.ENTITY_TRANSMUTATION;
 
@@ -147,6 +161,27 @@ public abstract class EntityMixin implements IEntity, SelfGetter<Entity> {
             }
             this.confluence$wasInShimmer = false;
         }
+    }
+
+    @Inject(method = "getName", at = @At("RETURN"), cancellable = true)
+    private void zombieName(CallbackInfoReturnable<Component> cir) {
+        Component returnValue = cir.getReturnValue();
+        Entity self = self();
+        if(self instanceof Zombie zombie){
+            ItemStack chestItem = zombie.getItemBySlot(EquipmentSlot.CHEST);
+            if (chestItem.is(ArmorItems.RAINCOAT.get())){
+                returnValue = Component.translatable("entity.confluence.raincoat_zombie");
+            } else if (chestItem.is(ArmorItems.SNOW_SUITS.get())
+                    || chestItem.is(ArmorItems.PINK_SNOW_SUITS.get())){
+                returnValue = Component.translatable("entity.confluence.frozen_zombie");
+            }
+        }
+        else if(self instanceof Skeleton skeleton){
+            if (skeleton.getItemBySlot(EquipmentSlot.CHEST).is(ArmorItems.MINING_CHESTPLATE.get())){
+                returnValue = Component.translatable("entity.confluence.undead_miner");
+            }
+        }
+        cir.setReturnValue(returnValue);
     }
 
     @Unique

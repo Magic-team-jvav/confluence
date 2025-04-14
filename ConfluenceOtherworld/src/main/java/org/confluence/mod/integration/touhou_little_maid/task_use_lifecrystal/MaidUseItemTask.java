116b073368b1f03aeb02e656089918c8b83100a6
westernat <westernat233@163.com>
2025-04-14 19:56:12 +0800

    整理待办

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/integration/touhou_little_maid/task_use_lifecrystal/MaidUseItemTask.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/integration/touhou_little_maid/task_use_lifecrystal/MaidUseItemTask.java
@@ -2,34 +2,34 @@ package org.confluence.mod.integration.touhou_little_maid.task_use_lifecrystal;
 
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.google.common.collect.ImmutableMap;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.behavior.Behavior;
 import net.minecraft.world.item.ItemStack;
-import org.confluence.mod.Confluence;
 import org.confluence.mod.common.init.ModAttachmentTypes;
 import org.confluence.mod.common.init.ModSoundEvents;
 import org.confluence.mod.common.init.item.ConsumableItems;
+import org.confluence.mod.common.item.common.EverBeneficialItem;
 
 public class MaidUseItemTask extends Behavior<EntityMaid> {
 
     int cooldown = 0;
     int _cooldown = 20;
+
     public MaidUseItemTask() {
         super(ImmutableMap.of(), 10);
 
     }
 
     protected boolean checkExtraStartConditions(ServerLevel worldIn, EntityMaid owner) {
-        if(--cooldown > 0){
+        if (--cooldown > 0) {
             return false;
         }
         cooldown = _cooldown;
         var data = owner.getData(ModAttachmentTypes.EVER_BENEFICIAL.get());
-        if(data.isLifeCrystalsMaximum()) return false;
+        if (data.isLifeCrystalsMaximum()) return false;
         ItemStack stack = owner.getItemBySlot(EquipmentSlot.MAINHAND);
         return stack.getItem() == ConsumableItems.LIFE_CRYSTAL.get();
     }
@@ -42,17 +42,10 @@ public class MaidUseItemTask extends Behavior<EntityMaid> {
         ItemStack stack = entityIn.getItemBySlot(EquipmentSlot.MAINHAND);
         if (stack.getItem() == ConsumableItems.LIFE_CRYSTAL.get()) {
             var data = entityIn.getData(ModAttachmentTypes.EVER_BENEFICIAL.get());
-            if(data.increaseCrystals()){
+            if (data.increaseCrystals()) {
                 var att = entityIn.getAttribute(Attributes.MAX_HEALTH);
-                if(att!= null) {
-                    String base = "life_crystal_health_modifier";
-                    int i = data.getUsedLifeCrystals();
-                    ResourceLocation location;
-                    do {
-                        location = Confluence.asResource(base + i);
-                        i++;
-                    }while (att.hasModifier(location));
-                    att.addPermanentModifier(new AttributeModifier(location, getStepIncrement(), AttributeModifier.Operation.ADD_VALUE));
+                if (att != null) {
+                    att.addOrReplacePermanentModifier(new AttributeModifier(EverBeneficialItem.LIFE_CRYSTAL.id(), getStepIncrement(), AttributeModifier.Operation.ADD_VALUE));
                     entityIn.playSound(ModSoundEvents.LIFE_CRYSTAL_USE.get());
                     entityIn.heal(getStepIncrement());
                     stack.shrink(1);

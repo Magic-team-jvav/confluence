2083f2b329cdf633ceaf72a48ceab812175e50d5
westernat <westernat233@163.com>
2025-04-15 22:55:36 +0800

    金墓石贴图

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/client/gui/TombstoneEditScreen.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/client/gui/TombstoneEditScreen.java
@@ -12,15 +12,18 @@ import org.joml.Vector3f;
 public class TombstoneEditScreen extends AbstractSignEditScreen {
     private static final Vector3f TEXT_SCALE = new Vector3f(1.5F, 1.5F, 1.5F);
     private static final ResourceLocation BACKGROUND = Confluence.asResource("textures/gui/tombstone.png");
+    private static final ResourceLocation GOLD_BACKGROUND = Confluence.asResource("textures/gui/gold_tombstone.png");
+    private final boolean isGold;
 
-    public TombstoneEditScreen(SignBlockEntity sign, boolean isFrontText, boolean isFiltered) {
+    public TombstoneEditScreen(SignBlockEntity sign, boolean isFrontText, boolean isFiltered, boolean isGold) {
         super(sign, isFrontText, isFiltered, Component.empty());
+        this.isGold = isGold;
     }
 
     @Override
     protected void renderSignBackground(GuiGraphics guiGraphics, BlockState state) {
         guiGraphics.pose().translate(0, 31, 0);
-        guiGraphics.blit(BACKGROUND, -111, -100, 0, 0, 223, 166);
+        guiGraphics.blit(isGold ? GOLD_BACKGROUND : BACKGROUND, -111, -100, 0, 0, 223, 166);
     }
 
     @Override
--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/client/entity/LocalPlayerMixin.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/client/entity/LocalPlayerMixin.java
@@ -1,10 +1,13 @@
 package org.confluence.mod.mixin.client.entity;
 
+import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
 import org.confluence.mod.client.gui.TombstoneEditScreen;
 import org.confluence.mod.common.block.common.TombstoneBlock;
+import org.confluence.mod.common.init.block.ModBlocks;
 import org.confluence.mod.mixed.ILocalPlayer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -35,7 +38,11 @@ public abstract class LocalPlayerMixin implements ILocalPlayer {
     @Inject(method = "openTextEdit", at = @At("HEAD"), cancellable = true)
     private void openTombstoneEdit(SignBlockEntity signEntity, boolean isFrontText, CallbackInfo ci) {
         if (signEntity instanceof TombstoneBlock.Entity entity) {
-            minecraft.setScreen(new TombstoneEditScreen(entity, isFrontText, minecraft.isTextFilteringEnabled()));
+            Block block = entity.getBlockState().getBlock();
+            boolean isGold = ModBlocks.TOMBSTONES.object2BooleanEntrySet().stream()
+                    .filter(entry -> entry.getKey().get() == block)
+                    .findAny().map(Object2BooleanMap.Entry::getBooleanValue).orElse(false);
+            minecraft.setScreen(new TombstoneEditScreen(entity, isFrontText, minecraft.isTextFilteringEnabled(), isGold));
             ci.cancel();
         }
     }

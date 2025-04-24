07a58f83e68782b99cdca2ffeb645343ab420047
westernat <westernat233@163.com>
2025-04-24 15:18:33 +0800

    扩大地下丛林规模

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/init/ModCarvers.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/init/ModCarvers.java
@@ -1,6 +1,8 @@
 package org.confluence.mod.common.init;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.level.levelgen.carver.CaveCarverConfiguration;
+import net.minecraft.world.level.levelgen.carver.CaveWorldCarver;
 import net.minecraft.world.level.levelgen.carver.WorldCarver;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import org.confluence.mod.Confluence;
@@ -19,4 +21,5 @@ public final class ModCarvers {
     public static final Supplier<WavyCaveCarver> WAVY_CAVE_CARVER = CARVERS.register("wavy_cave_carver", () -> new WavyCaveCarver(WavyCaveCarver.Config.CODEC));
     public static final Supplier<DesertCaveCarver> DESERT_CAVE_CARVER = CARVERS.register("desert_cave_carver", () -> new DesertCaveCarver(DesertCaveCarver.Config.CODEC));
     public static final Supplier<GlowingMushroomCaveCarver> GLOWING_MUSHROOM_CAVE_CARVER = CARVERS.register("glowing_mushroom_cave_carver", () -> new GlowingMushroomCaveCarver(GlowingMushroomCaveCarver.Config.CODEC));
+    public static final Supplier<CaveWorldCarver> JUNGLE_CAVE_CARVER = CARVERS.register("jungle_cave_carver", () -> new CaveWorldCarver(CaveCarverConfiguration.CODEC));
 }
--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/worldgen/carver/WavyCaveCarver.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/worldgen/carver/WavyCaveCarver.java
@@ -2,7 +2,6 @@ package org.confluence.mod.common.worldgen.carver;
 
 import com.google.common.collect.Lists;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.util.Mth;
@@ -61,9 +60,7 @@ public class WavyCaveCarver extends WorldCarver<WavyCaveCarver.Config> {
     }
 
     public static class Config extends CarverConfiguration {
-        public static final Codec<Config> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-                CarverConfiguration.CODEC.forGetter(config -> config)
-        ).apply(instance, Config::new));
+        public static final Codec<Config> CODEC = CarverConfiguration.CODEC.codec().xmap(Config::new, Function.identity());
 
         public Config(CarverConfiguration configuration) {
             super(configuration.probability, configuration.y, configuration.yScale, configuration.lavaLevel, configuration.debugSettings, configuration.replaceable);

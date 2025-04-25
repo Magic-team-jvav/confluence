73bac4b2eca3b0a99e59b1f8fcb87c27245ff154
the_xu <448537509@qq.com>
2025-04-25 23:11:48 +0800

    优化代码结构，把npc渲染器扩展到Mob

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/menu/NPCTradesForgeMenu.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/menu/NPCTradesForgeMenu.java
@@ -2,8 +2,11 @@ package org.confluence.mod.common.menu;
 
 import net.minecraft.world.entity.player.Inventory;
 import org.confluence.mod.common.init.ModMenuTypes;
+import org.confluence.terraentity.entity.npc.AbstractTerraNPC;
 import org.confluence.terraentity.entity.npc.trade.ITradeHolder;
+import org.confluence.terraentity.init.entity.TENpcEntities;
 import org.confluence.terraentity.menu.TETradesMenu;
+import org.confluence.terraentity.mixed.IPlayer;
 import org.jetbrains.annotations.Nullable;
 
 public class NPCTradesForgeMenu extends TETradesMenu {
@@ -12,6 +15,9 @@ public class NPCTradesForgeMenu extends TETradesMenu {
 
     public NPCTradesForgeMenu(int containerId, Inventory playerInventory) {
         this(containerId, playerInventory, null, false);
+        if(((IPlayer)playerInventory.player).terra_entity$getTradeHolder() instanceof AbstractTerraNPC npc && npc.getType() == TENpcEntities.GOBLIN_TINKERER.get()){
+            this.forge = true;
+        }
 
     }
 

e451bfcf8df9080c62eb6031092245d1b6044c70
XiaoHuNao <shiwenbin2250@gmail.com>
2025-04-27 18:17:21 +0800

    遗漏的重命名

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/equipment_set/SpiderSet.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/equipment_set/SpiderSet.java
@@ -1,8 +1,8 @@
 package org.confluence.mod.common.equipment_set;
 
 import com.xiaohunao.equipment_benediction.common.equipment_set.EquipmentSet;
+import com.xiaohunao.equipment_benediction.common.equipment_set.EquipmentSetBranch;
 import com.xiaohunao.equipment_benediction.common.equipment_set.EquippableGroup;
-import com.xiaohunao.equipment_benediction.common.equipment_set.EquippableSetData;
 import com.xiaohunao.equipment_benediction.common.equippable.VanillaEquippable;
 import com.xiaohunao.equipment_benediction.common.hook.HookMap;
 import com.xiaohunao.equipment_benediction.common.init.EBHookTypes;
@@ -15,27 +15,27 @@ import org.confluence.terraentity.init.TEAttributes;
 public class SpiderSet extends EquipmentSet {
     @Override
     protected void init(HookMap.Builder hook, EquippableGroup.Builder equippableGroup) {
-        equippableGroup.addEquippableSet("helmet", new EquippableSetData.Builder()
+        equippableGroup.addEquippableSet("helmet", new EquipmentSetBranch.Builder()
                 .addEquippable(VanillaEquippable.HEAD, ArmorItems.SPIDER_HELMET)
                 .bindHook(builder -> builder
                         .addBonus(TEAttributes.MINION_CAPACITY, new AttributeModifier(ArmorItems.SPIDER_HELMET.getId(), 1, AttributeModifier.Operation.ADD_VALUE))
                         .addBonus(TEAttributes.SUMMON_DAMAGE, new AttributeModifier(ArmorItems.SPIDER_HELMET.getId(), 0.05, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL))
                 ).build());
-        equippableGroup.addEquippableSet("chestplate", new EquippableSetData.Builder()
+        equippableGroup.addEquippableSet("chestplate", new EquipmentSetBranch.Builder()
                 .addEquippable(VanillaEquippable.CHEST, ArmorItems.SPIDER_CHESTPLATE)
                 .bindHook(builder -> builder
                         .addBonus(TEAttributes.MINION_CAPACITY, new AttributeModifier(ArmorItems.SPIDER_CHESTPLATE.getId(), 1, AttributeModifier.Operation.ADD_VALUE))
                         .addBonus(TEAttributes.SUMMON_DAMAGE, new AttributeModifier(ArmorItems.SPIDER_CHESTPLATE.getId(), 0.05, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL))
                 ).build());
-        equippableGroup.addEquippableSet("leggings", new EquippableSetData.Builder()
+        equippableGroup.addEquippableSet("leggings", new EquipmentSetBranch.Builder()
                 .addEquippable(VanillaEquippable.LEGS, ArmorItems.SPIDER_LEGGINGS)
                 .bindHook(builder -> builder.addBonus(TEAttributes.MINION_CAPACITY, new AttributeModifier(ArmorItems.SPIDER_LEGGINGS.getId(), 1, AttributeModifier.Operation.ADD_VALUE))).build());
-        equippableGroup.addEquippableSet("boots", new EquippableSetData.Builder()
+        equippableGroup.addEquippableSet("boots", new EquipmentSetBranch.Builder()
                 .addEquippable(VanillaEquippable.FEET, ArmorItems.SPIDER_BOOTS)
                 .bindHook(builder -> builder.addBonus(TEAttributes.SUMMON_DAMAGE, new AttributeModifier(ArmorItems.SPIDER_BOOTS.getId(), 0.06, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)))
                 .build());
 
-        equippableGroup.addEquippableSet("full_set", new EquippableSetData.Builder()
+        equippableGroup.addEquippableSet("full_set", new EquipmentSetBranch.Builder()
                 .addEquippable(
                         VanillaEquippable.HEAD, ArmorItems.SPIDER_HELMET,
                         VanillaEquippable.CHEST, ArmorItems.SPIDER_CHESTPLATE,

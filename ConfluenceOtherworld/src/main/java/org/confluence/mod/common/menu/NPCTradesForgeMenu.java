34e9b488fcf9faa6eff809acdbda6723b1e190f3
the_xu <448537509@qq.com>
2025-05-04 13:47:27 +0800

    修复npc吞物品

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/menu/NPCTradesForgeMenu.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/menu/NPCTradesForgeMenu.java
@@ -2,6 +2,7 @@ package org.confluence.mod.common.menu;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.confluence.mod.common.init.ModMenuTypes;
@@ -18,15 +19,16 @@ public class NPCTradesForgeMenu extends TETradesMenu {
 
     public NPCTradesForgeMenu(int containerId, Inventory playerInventory) {
         this(containerId, playerInventory, null, false);
-        ITradeHolder holder = ((IPlayer)playerInventory.player).terra_entity$getTradeHolder();
-        if (holder instanceof AbstractTerraNPC npc && npc.getType() == TENpcEntities.GOBLIN_TINKERER.get()){
+        ITradeHolder holder = ((IPlayer) playerInventory.player).terra_entity$getTradeHolder();
+        if (holder instanceof AbstractTerraNPC npc && npc.getType() == TENpcEntities.GOBLIN_TINKERER.get()) {
             this.forge = true;
-        }else{
+        } else {
             try {
-                if(holder != null && holder.getClass().isAssignableFrom(Class.forName("com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid"))) {
+                if (holder != null && holder.getClass().isAssignableFrom(Class.forName("com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid"))) {
                     this.forge = true;
                 }
-            } catch (ClassNotFoundException ignored) {}
+            } catch (ClassNotFoundException ignored) {
+            }
         }
 
     }
@@ -36,14 +38,15 @@ public class NPCTradesForgeMenu extends TETradesMenu {
         this.forge = forge;
     }
 
-    protected void addResultSlot(){
-        this.addSlot(new Slot(this.container, 0, 238, 37){
+    protected void addResultSlot() {
+        this.addSlot(new Slot(this.container, 0, 238, 37) {
             @Override
             public boolean mayPlace(ItemStack stack) {
                 return true;
             }
+
             @Override
-            public void onTake(Player player, ItemStack stack){
+            public void onTake(Player player, ItemStack stack) {
 //                var d  = ((IPlayer)playerInventory.player).terra_entity$getDaveTrades();
 //                if(selectedMerchantIndex >= 0 && selectedMerchantIndex < d.trades().size()){
 //                    PacketDistributor.sendToServer(new NPCShopPacket((ITrade) d.trades().get(selectedMerchantIndex)));
@@ -53,4 +56,8 @@ public class NPCTradesForgeMenu extends TETradesMenu {
         });
     }
 
+    public void clicked(int slotId, int button, ClickType clickType, Player player) {
+
+        super.clicked(slotId, button, clickType, player);
+    }
 }

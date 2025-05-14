b92b020a5de8875ecf062277438e40d72ff3e9ba
westernat <westernat233@163.com>
2025-05-14 17:29:21 +0800

    本体存在时，队伍按钮显示到额外槽

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/integration/jei/ExtraInventoryHandler.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/integration/jei/ExtraInventoryHandler.java
@@ -7,13 +7,17 @@ import org.confluence.mod.client.gui.container.ExtraInventoryScreen;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class ExtraInventoryHandler implements IGlobalGuiHandler {
     @Override
     public Collection<Rect2i> getGuiExtraAreas() {
         Minecraft minecraft = Minecraft.getInstance();
         if (minecraft.screen instanceof ExtraInventoryScreen screen) {
-            return Collections.singletonList(new Rect2i(screen.getGuiLeft() - 33, screen.getGuiTop(), 32, 256));
+            return List.of(
+                    new Rect2i(screen.getGuiLeft() - 33, screen.getGuiTop(), 32, 256),
+                    new Rect2i(screen.getGuiLeft() + 171, screen.getGuiTop(), 16, 256)
+            );
         }
         return Collections.emptyList();
     }

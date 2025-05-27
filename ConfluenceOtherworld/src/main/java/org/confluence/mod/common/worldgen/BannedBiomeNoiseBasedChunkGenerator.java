48ab785924ec7d479d23704b36fcce8c40811b8f
westernat <westernat233@163.com>
2025-05-27 18:04:21 +0800

    修banned biome source

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/worldgen/BannedBiomeNoiseBasedChunkGenerator.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/worldgen/BannedBiomeNoiseBasedChunkGenerator.java
@@ -9,13 +9,14 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import org.confluence.lib.common.data.IdFixer;
 
 public class BannedBiomeNoiseBasedChunkGenerator extends NoiseBasedChunkGenerator {
     public static final MapCodec<BannedBiomeNoiseBasedChunkGenerator> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             MultiNoiseBiomeSource.CODEC.fieldOf("biome_source").forGetter(BannedBiomeNoiseBasedChunkGenerator::getBiomeSource),
             NoiseGeneratorSettings.CODEC.fieldOf("settings").forGetter(BannedBiomeNoiseBasedChunkGenerator::generatorSettings),
-            ResourceKey.codec(Registries.BIOME).fieldOf("banned_biome").forGetter(generator -> generator.bannedBiome),
-            ResourceKey.codec(Registries.BIOME).fieldOf("target_biome").forGetter(generator -> generator.targetBiome)
+            IdFixer.fixBiomeKey(ResourceKey.codec(Registries.BIOME)).fieldOf("banned_biome").forGetter(generator -> generator.bannedBiome),
+            IdFixer.fixBiomeKey(ResourceKey.codec(Registries.BIOME)).fieldOf("target_biome").forGetter(generator -> generator.targetBiome)
     ).apply(instance, instance.stable(BannedBiomeNoiseBasedChunkGenerator::new)));
     public final ResourceKey<Biome> bannedBiome;
     public final ResourceKey<Biome> targetBiome;

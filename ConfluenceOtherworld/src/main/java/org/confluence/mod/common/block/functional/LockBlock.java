870ee9826ec46f2b7082929d1362f961fe1b93b4
westernat <westernat233@163.com>
2025-07-05 11:12:03 +0800

    锁方块

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/block/functional/LockBlock.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/block/functional/LockBlock.java
@@ -1,20 +1,67 @@
 package org.confluence.mod.common.block.functional;
 
+import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
+import net.minecraft.world.phys.BlockHitResult;
+import org.confluence.mod.common.init.block.FunctionalBlocks;
 
-// todo 匹配物品就破坏自身
+import java.util.Optional;
+
 public class LockBlock extends Block implements EntityBlock {
     public LockBlock(Properties properties) {
         super(properties);
     }
 
     @Override
-    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return null;
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new Entity(pos, state);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        if (!level.isClientSide && level.getBlockEntity(pos) instanceof Entity entity && (entity.matchTool.isEmpty() || entity.matchTool.get().test(stack)) && level.destroyBlock(pos, false, player)) {
+            if (entity.consumeTool) {
+                stack.shrink(1);
+                return ItemInteractionResult.CONSUME;
+            }
+            return ItemInteractionResult.CONSUME_PARTIAL;
+        }
+        return ItemInteractionResult.SUCCESS;
+    }
+
+    public static class Entity extends BlockEntity {
+        private Optional<ItemPredicate> matchTool = Optional.empty();
+        private boolean consumeTool = false;
+
+        public Entity(BlockPos pos, BlockState blockState) {
+            super(FunctionalBlocks.LOCK_BLOCK_ENTITY.get(), pos, blockState);
+        }
+
+        @Override
+        protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+            super.loadAdditional(tag, registries);
+            this.matchTool = ItemPredicate.CODEC.parse(RegistryOps.create(NbtOps.INSTANCE, registries), tag.get("MatchTool")).result();
+            this.consumeTool = tag.getBoolean("ConsumeTool");
+        }
+
+        @Override
+        protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+            super.saveAdditional(tag, registries);
+            matchTool.flatMap(predicate -> ItemPredicate.CODEC.encodeStart(RegistryOps.create(NbtOps.INSTANCE, registries), predicate).result()).ifPresent(nbt -> tag.put("MatchTool", nbt));
+            tag.putBoolean("ConsumeTool", consumeTool);
+        }
     }
 }

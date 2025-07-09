18240c251f4157542c664023c4d9816f90eee563
westernat <westernat233@163.com>
2025-07-09 12:42:55 +0800

    骷髅王有手的时候头部限伤

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/integration/terra_entity/SkeletronMixin.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/integration/terra_entity/SkeletronMixin.java
@@ -1,10 +1,14 @@
 package org.confluence.mod.mixin.integration.terra_entity;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.Holder;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import org.confluence.mod.common.init.ModEffects;
+import org.confluence.mod.mixed.IDamageSource;
 import org.confluence.terraentity.entity.boss.Skeletron;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,5 +23,11 @@ public abstract class SkeletronMixin {
         if (effect == ModEffects.TENTACLE_SPIKES || effect == ModEffects.BLOOD_BUTCHERED) { // todo 穿透
             cir.setReturnValue(false);
         }
+    }
+
+    @WrapOperation(method = "hurt", at = @At(value = "INVOKE", target = "Lorg/confluence/terraentity/entity/boss/AbstractTerraBossBase;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"))
+    private boolean limitDamage(Skeletron instance, DamageSource damageSource, float amount, Operation<Boolean> original) {
+        if (instance.hands.isEmpty()) return original.call(instance, damageSource, amount);
+        return original.call(instance, damageSource, ((IDamageSource) damageSource).confluence$isCritical() ? 2.0F : 1.0F);
     }
 }

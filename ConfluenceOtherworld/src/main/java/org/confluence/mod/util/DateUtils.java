7bf5328686de22866ef342b83422b47466870e65
westernat <westernat233@163.com>
2025-08-09 23:44:56 +0800

    保证其它使用本体date util的模组找得到方法

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/util/DateUtils.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/util/DateUtils.java
@@ -4,6 +4,8 @@ import com.nlf.calendar.Lunar;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.confluence.lib.util.LibDateUtils;
 import org.confluence.mod.common.init.item.ConsumableItems;
 import org.confluence.mod.common.init.item.FoodItems;
 import org.confluence.mod.common.init.item.ModItems;
@@ -12,6 +14,11 @@ import java.util.Calendar;
 import java.util.Date;
 
 public final class DateUtils {
+    public static final @Deprecated int _00$00 = LibDateUtils._00$00;
+    public static final @Deprecated int _04$30 = LibDateUtils._04$30;
+    public static final @Deprecated int _06$00 = LibDateUtils._06$00;
+    public static final @Deprecated int _19$30 = LibDateUtils._19$30;
+
     private static long lastCacheTime = 0;
     private static final Calendar calendar = Calendar.getInstance();
     private static Lunar lunar;
@@ -86,5 +93,40 @@ public final class DateUtils {
         if (isHalloween(calendar)) return ModItems.SOUL_CAKE.get();
         if (isChristmas(calendar)) return ModItems.SUGAR_PLUM.get();
         return ModItems.STAR.get();
+    }
+
+    @Deprecated
+    public static int getDayTime(Level level) {
+        return LibDateUtils.getDayTime(level);
+    }
+
+    @Deprecated
+    public static int getDayTime(long dayTime) {
+        return LibDateUtils.getDayTime(dayTime);
+    }
+
+    @Deprecated
+    public static int getDayTime(int hour, int minute) {
+        return LibDateUtils.getDayTime(hour, minute);
+    }
+
+    @Deprecated
+    public static boolean isWithinDayTime(int start, int end, int dayTime) {
+        return LibDateUtils.isWithinDayTime(start, end, dayTime);
+    }
+
+    @Deprecated
+    public static boolean isWithinDayTime(int startHour, int startMinute, int endHour, int endMinute, int dayTime) {
+        return LibDateUtils.isWithinDayTime(startHour, startMinute, endHour, endMinute, dayTime);
+    }
+
+    @Deprecated
+    public static boolean isDay(int dayTime) {
+        return LibDateUtils.isDay(dayTime);
+    }
+
+    @Deprecated
+    public static boolean isNight(int dayTime) {
+        return LibDateUtils.isNight(dayTime);
     }
 }

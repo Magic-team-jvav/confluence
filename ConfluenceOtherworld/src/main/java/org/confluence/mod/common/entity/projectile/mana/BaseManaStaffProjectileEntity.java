42ade9ff5104e3d291bf81440ad15d3fc17edde1
westernat <westernat233@163.com>
2025-10-01 18:25:52 +0800

    修复非宝石射弹崩溃

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/entity/projectile/mana/BaseManaStaffProjectileEntity.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/entity/projectile/mana/BaseManaStaffProjectileEntity.java
@@ -94,7 +94,6 @@ public class BaseManaStaffProjectileEntity extends AbstractManaProjectile {
         if (level().isClientSide && emitter == null) {
             Variant variant = getVariant();
             ResourceLocation particleId;
-            MolangExp expression = MolangExp.EMPTY;
             if (variant == Variant.FROST) {
                 particleId = Confluence.asResource("frost_projectile");
             } else if (variant == Variant.SPARK) {
@@ -103,13 +102,12 @@ public class BaseManaStaffProjectileEntity extends AbstractManaProjectile {
                 particleId = Confluence.asResource("thunder_zapper");
             } else {
                 particleId = Confluence.asResource("base_mana_staff_projectile");
-                expression = new MolangExp(Map.of(
-                        "variable.red", Float.toString(variant.color.red()),
-                        "variable.green", Float.toString(variant.color.green()),
-                        "variable.blue", Float.toString(variant.color.blue())
-                ));
             }
-            this.emitter = new ParticleEmitter(level(), position(), particleId, expression);
+            this.emitter = new ParticleEmitter(level(), position(), particleId, new MolangExp(Map.of(
+                    "variable.red", Float.toString(variant.color.red()),
+                    "variable.green", Float.toString(variant.color.green()),
+                    "variable.blue", Float.toString(variant.color.blue())
+            )));
             emitter.attachEntity(this);
             PSGameClient.LOADER.addEmitter(emitter, false);
         }

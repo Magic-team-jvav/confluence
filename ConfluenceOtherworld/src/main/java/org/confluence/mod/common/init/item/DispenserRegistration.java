b61166223cb7a8680e3ee8d26e68a0c4a1e015f3
MakerTechno <102357646+makertechno@users.noreply.github.com>
2025-12-02 18:51:37 +0800

    海绵适配发射器

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/init/item/DispenserRegistration.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/init/item/DispenserRegistration.java
@@ -9,6 +9,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
+import org.confluence.mod.common.item.common.SpongeItem;
 import org.jetbrains.annotations.NotNull;
 
 public class DispenserRegistration {
@@ -42,10 +43,25 @@ public class DispenserRegistration {
                 }
             }
         };
+        final DispenseItemBehavior spongeAbsorbLiquid = new DefaultDispenseItemBehavior() {
+            @Override
+            public @NotNull ItemStack execute(BlockSource source, ItemStack itemStack) {
+                SpongeItem spongeItem = (SpongeItem) itemStack.getItem();
+                BlockPos blockpos = source.pos().relative(source.state().getValue(DispenserBlock.FACING));
+                Level level = source.level();
+                spongeItem.tryAbsorbLiquid(level, blockpos);
+                return itemStack;
+            }
+        };
         DispenserBlock.registerBehavior(ToolItems.HONEY_BUCKET.asItem(), simpleFluidOut);
         DispenserBlock.registerBehavior(ToolItems.BOTTOMLESS_WATER_BUCKET.asItem(), bottomlessFluidOut);
         DispenserBlock.registerBehavior(ToolItems.BOTTOMLESS_LAVA_BUCKET.asItem(), bottomlessFluidOut);
         DispenserBlock.registerBehavior(ToolItems.BOTTOMLESS_HONEY_BUCKET.asItem(), bottomlessFluidOut);
         DispenserBlock.registerBehavior(ToolItems.BOTTOMLESS_SHIMMER_BUCKET.asItem(), bottomlessFluidOut);
+
+        DispenserBlock.registerBehavior(ToolItems.SUPER_ABSORBANT_SPONGE.asItem(), spongeAbsorbLiquid);
+        DispenserBlock.registerBehavior(ToolItems.HONEY_ABSORBANT_SPONGE.asItem(), spongeAbsorbLiquid);
+        DispenserBlock.registerBehavior(ToolItems.LAVA_ABSORBANT_SPONGE.asItem(), spongeAbsorbLiquid);
+        DispenserBlock.registerBehavior(ToolItems.ULTRA_ABSORBANT_SPONGE.asItem(), spongeAbsorbLiquid);
     }
 }

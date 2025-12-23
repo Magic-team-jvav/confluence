2169de41e6428f3c35c5bb96cfa878fca818e1fa
voila <voila1106@gmail.com>
2025-12-23 20:33:04 +0800

    拆分暴击mixin

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixed/IDamageSource.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixed/IDamageSource.java
@@ -4,12 +4,10 @@ import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
+import org.confluence.lib.mixed.CriticalDamageSource;
 import org.confluence.lib.mixed.SelfGetter;
 
-public interface IDamageSource extends SelfGetter<DamageSource> {
-    void confluence$setCritical(boolean critical);
-
-    boolean confluence$isCritical();
+public interface IDamageSource extends CriticalDamageSource, SelfGetter<DamageSource> {
 
     default boolean confluence$checkBypassesCooldown(TagKey<DamageType> damageTypeKey) {
         return damageTypeKey == DamageTypeTags.BYPASSES_COOLDOWN && Immunity.getCause(confluence$self()) != null;

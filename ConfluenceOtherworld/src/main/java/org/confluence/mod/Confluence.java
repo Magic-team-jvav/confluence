d890d34b00d6bf3f3d74349a90fe05db24954bfd
westernat <westernat233@163.com>
2026-01-03 20:28:42 +0800

    测试iris的彩色光照

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/Confluence.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/Confluence.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.GameRules;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.LoadingModList;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.confluence.lib.ConfluenceMagicLib;
@@ -23,6 +24,7 @@ import org.confluence.mod.common.init.block.ModBlocks;
 import org.confluence.mod.common.init.item.ModItems;
 import org.confluence.mod.integration.create.CreateHelper;
 import org.confluence.mod.integration.heaven_destiny_moment.HDMEvents;
+import org.confluence.mod.integration.sodium.iris.IrisHelper;
 import org.confluence.mod.integration.terra_entity.TEEvents;
 import org.confluence.mod.integration.terra_entity.init.ModTradeLockProviderTypes;
 import org.confluence.mod.integration.terra_furniture.TFReferences;
@@ -56,6 +58,9 @@ public final class Confluence {
         WaystonesHelper.register(eventBus);
         CreateHelper.register(eventBus);
         ModAdvancements.register(eventBus);
+        if (LoadingModList.get().getModFileById("iris") != null) {
+            IrisHelper.register(eventBus);
+        }
 
         TFReferences.init();
         DifficultyScaler.init();

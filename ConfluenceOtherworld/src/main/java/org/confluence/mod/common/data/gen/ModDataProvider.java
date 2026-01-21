d787b79013a1744cb608baa5abe01ee15bed0cd5
magicharp <2749380016@qq.com>
2026-01-21 20:52:14 +0800

    微调生成x3

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/data/gen/ModDataProvider.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/data/gen/ModDataProvider.java
@@ -1233,7 +1233,7 @@ public class ModDataProvider {
             register(context, createModifierKey("common_overworld"), new BiomeModifiers.AddSpawnsBiomeModifier(
                     overworld,
                     List.of(
-                            new MobSpawnSettings.SpawnerData(TEMonsterEntities.NYMPH.get(), 2, 1, 1),
+                            new MobSpawnSettings.SpawnerData(TEMonsterEntities.NYMPH.get(), 3, 1, 1),
                             new MobSpawnSettings.SpawnerData(TEAnimals.FAIRY.get(), 3, 1, 1),
                             new MobSpawnSettings.SpawnerData(TEMonsterEntities.HARPY.get(), 60, 1, 2),
                             new MobSpawnSettings.SpawnerData(TEMonsterEntities.WYVERN.get(), 8, 1, 1)

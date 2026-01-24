83ce7fe516500386250b6e7ec152175ff71a7194
westernat <westernat233@163.com>
2026-01-25 00:25:58 +0800

    更改地牢指南针模型

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/client/renderer/item/DungeonCompassRenderer.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/client/renderer/item/DungeonCompassRenderer.java
@@ -2,28 +2,42 @@ package org.confluence.mod.client.renderer.item;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.SkullModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.SkullBlock;
-import net.minecraft.world.phys.Vec3;
 import org.confluence.lib.util.LibUtils;
+import org.confluence.mod.Confluence;
 import org.confluence.mod.common.init.item.ToolItems;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+import software.bernie.geckolib.cache.GeckoLibCache;
+import software.bernie.geckolib.cache.object.*;
+import software.bernie.geckolib.util.RenderUtil;
 
 public class DungeonCompassRenderer {
     private static DungeonCompassRenderer INSTANCE;
-    private final SkullModel model;
+    private final GeoBone bone;
+    private final GeoBone outline;
+    private final RenderType boneRenderType;
+    private final RenderType outlineRenderType;
 
     private DungeonCompassRenderer() {
-        this.model = new SkullModel(Minecraft.getInstance().getEntityModels().bakeLayer(ModelLayers.SKELETON_SKULL));
+        BakedGeoModel geoModel = GeckoLibCache.getBakedModels().get(Confluence.asResource("geo/item/dungeon_compass.geo.json"));
+        this.bone = geoModel.getBone("bone").orElseThrow();
+        this.outline = geoModel.getBone("outline").orElseThrow();
+        ResourceLocation texture = Confluence.asResource("textures/item/dungeon_compass.png");
+        this.boneRenderType = RenderType.text(texture);
+        this.outlineRenderType = RenderType.eyes(texture);
     }
 
     public static DungeonCompassRenderer getInstance() {
@@ -56,20 +70,58 @@ public class DungeonCompassRenderer {
         if (!Minecraft.getInstance().options.getCameraType().isFirstPerson()) return;
         RenderSystem.enableDepthTest();
         poseStack.pushPose();
-        poseStack.scale(-1, -1, 1);
-        float ry = Mth.PI - player.getYRot() * Mth.DEG_TO_RAD;
+        float ry = Mth.PI + player.getYRot() * Mth.DEG_TO_RAD;
         float cos = Mth.cos(ry);
         float sin = Mth.sin(ry);
-        double mx = -0.75;
-        double mz = -1;
-        double vx = mx * cos + mz * -sin;
-        double vz = mx * sin + mz * cos;
-        poseStack.translate(vx, 0.25, vz);
-        Vec3 delta = new Vec3(x - player.getX(), 0, z - player.getZ());
-        float yaw = (float) Mth.atan2(-delta.x, delta.z) * Mth.RAD_TO_DEG;
-        model.setupAnim(0, 180 + yaw, 0);
-        model.renderToBuffer(poseStack, bufferSource.getBuffer(SkullBlockRenderer.getRenderType(SkullBlock.Types.SKELETON, null)), 0xF000F0, OverlayTexture.NO_OVERLAY);
+        float mx = 0.75F;
+        float mz = -1F;
+        float vx = mx * cos + mz * -sin;
+        float vz = mx * sin + mz * cos;
+        poseStack.translate(vx, 0.15F, vz);
+        float yaw = Mth.PI - (float) Mth.atan2(-x + player.getX(), z - player.getZ());
+
+        renderGeoBone(bone, boneRenderType, poseStack, bufferSource, yaw, 0);
+        renderGeoBone(outline, outlineRenderType, poseStack, bufferSource, yaw, -1);
+
         poseStack.popPose();
         RenderSystem.disableDepthTest();
+    }
+
+    private static void renderGeoBone(GeoBone geoBone, RenderType renderType, PoseStack poseStack, MultiBufferSource bufferSource, float yaw, float yOffset) {
+        VertexConsumer buffer;
+        poseStack.pushPose();
+        if (yOffset != 0) {
+            geoBone.updatePosition(0, yOffset, 0);
+        }
+        geoBone.setRotY(yaw);
+        geoBone.updateScale(0.8F, 0.8F, 0.8F);
+        RenderUtil.prepMatrixForBone(poseStack, geoBone);
+        buffer = bufferSource.getBuffer(renderType);
+        for (GeoCube cube : geoBone.getCubes()) {
+            poseStack.pushPose();
+            RenderUtil.translateToPivotPoint(poseStack, cube);
+            RenderUtil.rotateMatrixAroundCube(poseStack, cube);
+            RenderUtil.translateAwayFromPivotPoint(poseStack, cube);
+
+            Matrix3f normalisedPoseState = poseStack.last().normal();
+            Matrix4f poseState = new Matrix4f(poseStack.last().pose());
+
+            for (GeoQuad quad : cube.quads()) {
+                if (quad == null) continue;
+
+                Vector3f normal = normalisedPoseState.transform(new Vector3f(quad.normal()));
+
+                RenderUtil.fixInvertedFlatCube(cube, normal);
+                for (GeoVertex vertex : quad.vertices()) {
+                    Vector3f position = vertex.position();
+                    Vector4f vector4f = poseState.transform(new Vector4f(position.x(), position.y(), position.z(), 1.0f));
+
+                    buffer.addVertex(vector4f.x(), vector4f.y(), vector4f.z(), -1, vertex.texU(),
+                            vertex.texV(), OverlayTexture.NO_OVERLAY, 0xF000F0, normal.x(), normal.y(), normal.z());
+                }
+            }
+            poseStack.popPose();
+        }
+        poseStack.popPose();
     }
 }

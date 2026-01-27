162d994245e6f55bdbe9f015fd8c9e8b5ad62b79
westernat <westernat233@163.com>
2026-01-27 14:48:38 +0800

    修复蠕虫生成造成的线程死锁

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/integration/terra_entity/TEHelper.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/integration/terra_entity/TEHelper.java
@@ -9,6 +9,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import org.confluence.mod.Confluence;
 import org.confluence.mod.util.ModUtils;
@@ -36,9 +37,10 @@ public final class TEHelper {
 
     /// [蠕虫](https://terraria.wiki.gg/zh/wiki/%E8%A0%95%E8%99%AB)
     public static boolean wormSpawnRules(EntityType<SimpleVariantAnimal> type, ServerLevelAccessor level, MobSpawnType spawnType, BlockPos pos, RandomSource random) {
+        if (!(level instanceof Level level1)) return false;
         int y = pos.getY();
         int surfaceY = OverworldUtils.getSurfaceY();
-        if (y > surfaceY && y < OverworldUtils.getSpaceY() && ModUtils.isRainingAt(level.getLevel(), pos)) {
+        if (y > surfaceY && y < OverworldUtils.getSpaceY() && ModUtils.isRainingAt(level1, pos)) {
             return true;
         }
         return y > OverworldUtils.getUndergroundY() && y < surfaceY;

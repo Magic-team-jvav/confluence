f127d8652c7addd5f88a3014692629f9b6b6e874
王小尽 <1809315974@qq.com>
2026-02-21 12:56:21 +0800

    汇流玻璃瓶转换到汇流水玻璃瓶

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/init/item/DispenserRegistration.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/init/item/DispenserRegistration.java
@@ -1,16 +1,16 @@
 package org.confluence.mod.common.init.item;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.dispenser.BlockSource;
-import net.minecraft.core.dispenser.BoatDispenseItemBehavior;
-import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
-import net.minecraft.core.dispenser.DispenseItemBehavior;
+import net.minecraft.core.dispenser.*;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.world.item.BoatItem;
 import net.minecraft.world.item.DispensibleContainerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import org.confluence.mod.common.item.common.SpongeItem;
 
@@ -20,7 +20,7 @@ public class DispenserRegistration {
         DispenseItemBehavior simpleFluidOut = new DefaultDispenseItemBehavior() {
             @Override
             public ItemStack execute(BlockSource source, ItemStack itemStack) {
-                DispensibleContainerItem dispensiblecontaineritem = (DispensibleContainerItem)itemStack.getItem();
+                DispensibleContainerItem dispensiblecontaineritem = (DispensibleContainerItem) itemStack.getItem();
                 BlockPos blockpos = source.pos().relative(source.state().getValue(DispenserBlock.FACING));
                 Level level = source.level();
                 if (dispensiblecontaineritem.emptyContents(null, level, blockpos, null, itemStack)) {
@@ -34,7 +34,7 @@ public class DispenserRegistration {
         DispenseItemBehavior bottomlessFluidOut = new DefaultDispenseItemBehavior() {
             @Override
             public ItemStack execute(BlockSource source, ItemStack itemStack) {
-                DispensibleContainerItem dispensiblecontaineritem = (DispensibleContainerItem)itemStack.getItem();
+                DispensibleContainerItem dispensiblecontaineritem = (DispensibleContainerItem) itemStack.getItem();
                 BlockPos blockpos = source.pos().relative(source.state().getValue(DispenserBlock.FACING));
                 Level level = source.level();
                 if (dispensiblecontaineritem.emptyContents(null, level, blockpos, null, itemStack)) {
@@ -79,5 +79,36 @@ public class DispenserRegistration {
         ArrowItems.ITEMS.getEntries().stream()
                 .map(DeferredHolder::get)
                 .forEach(DispenserBlock::registerProjectileBehavior);
+
+        DispenserBlock.registerBehavior(PotionItems.BOTTLE.asItem(), new OptionalDispenseItemBehavior() {
+            private ItemStack takeLiquid(BlockSource source, ItemStack emptyItem, ItemStack fullItem) {
+                source.level().gameEvent(null, GameEvent.FLUID_PICKUP, source.pos());
+                return this.consumeWithRemainder(source, emptyItem, fullItem);
+            }
+
+            @Override
+            public ItemStack execute(BlockSource blockSource, ItemStack item) {
+                this.setSuccess(false);
+                ServerLevel serverlevel = blockSource.level();
+                BlockPos blockpos = blockSource.pos().relative(blockSource.state().getValue(DispenserBlock.FACING));
+//                BlockState blockstate = serverlevel.getBlockState(blockpos);
+
+                // 蜂蜜 看下是否需要
+//                if (blockstate.is(BlockTags.BEEHIVES, p_338544_ -> p_338544_.hasProperty(BeehiveBlock.HONEY_LEVEL) &&
+//                        p_338544_.getBlock() instanceof BeehiveBlock) &&
+//                        blockstate.getValue(BeehiveBlock.HONEY_LEVEL) >= 5) {
+//                    ((BeehiveBlock) blockstate.getBlock()).releaseBeesAndResetHoneyLevel(serverlevel, blockstate, blockpos, null, BeehiveBlockEntity.BeeReleaseStatus.BEE_RELEASED);
+//                    this.setSuccess(true);
+//                    return this.takeLiquid(blockSource, item, new ItemStack(Items.HONEY_BOTTLE));
+//                }
+
+                if (serverlevel.getFluidState(blockpos).is(FluidTags.WATER)) {
+                    this.setSuccess(true);
+                    return this.takeLiquid(blockSource, item, PotionItems.BOTTLED_WATER.toStack());
+                }
+
+                return super.execute(blockSource, item);
+            }
+        });
     }
 }

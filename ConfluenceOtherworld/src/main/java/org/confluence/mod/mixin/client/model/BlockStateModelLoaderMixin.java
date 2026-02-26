cf1fb9144bd4ab9971c92c4116cdbb800cec5fa3
westernat <westernat233@163.com>
2026-02-26 11:53:44 +0800

    屏蔽另外两个大量输出的经过日志

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/client/model/BlockStateModelLoaderMixin.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/client/model/BlockStateModelLoaderMixin.java
@@ -5,18 +5,22 @@ import net.minecraft.client.resources.model.BlockStateModelLoader;
 import net.minecraft.resources.ResourceLocation;
 import org.confluence.mod.Confluence;
 import org.confluence.terra_furniture.TerraFurniture;
+import org.confluence.terraentity.TerraEntity;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Set;
 
 @Mixin(BlockStateModelLoader.class)
 public abstract class BlockStateModelLoaderMixin {
+    @Unique
+    private static final Set<String> confluence$skipSet = Set.of(Confluence.MODID, TerraFurniture.MODID, TerraEntity.MODID);
+
+    /// "Exception loading blockstate definition: '{}' missing model for variant: '{}'"
     @WrapWithCondition(method = "lambda$loadBlockStateDefinitions$10", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", ordinal = 0))
     private static boolean skipConfluenceLog(Logger instance, String s, Object o1, Object o2) {
-        if (o1 instanceof ResourceLocation rl) {
-            String namespace = rl.getNamespace();
-            return !Confluence.MODID.equals(namespace) && !TerraFurniture.MODID.equals(namespace);
-        }
-        return true;
+        return !(o1 instanceof ResourceLocation rl) || !confluence$skipSet.contains(rl.getNamespace());
     }
 }
---
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/mixin/client/model/ModelManagerMixin.java
@@ -0,0 +1,27 @@
+package org.confluence.mod.mixin.client.model;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import org.confluence.mod.Confluence;
+import org.confluence.terra_furniture.TerraFurniture;
+import org.confluence.terra_guns.TerraGuns;
+import org.confluence.terraentity.TerraEntity;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Set;
+
+@Mixin(ModelManager.class)
+public abstract class ModelManagerMixin {
+    @Unique
+    private static final Set<String> confluence$skipSet = Set.of(Confluence.MODID, TerraFurniture.MODID, TerraEntity.MODID, TerraGuns.MODID);
+
+    /// "Missing textures in model {}:\n{}"
+    @WrapWithCondition(method = "lambda$loadModels$17",at= @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
+    private static boolean skipConfluenceLog(Logger instance, String s, Object o1, Object o2) {
+        return !(o1 instanceof ModelResourceLocation rl) || !confluence$skipSet.contains(rl.id().getNamespace());
+    }
+}

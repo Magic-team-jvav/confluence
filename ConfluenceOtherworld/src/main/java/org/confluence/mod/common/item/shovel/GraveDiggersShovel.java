84c3724e741acbf33a165da5f9ef04db2acb4956
iceberries <1941265050@qq.com>
2026-06-15 18:41:23 +0800

    改一点

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/item/shovel/GraveDiggersShovel.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/item/shovel/GraveDiggersShovel.java
@@ -1,5 +1,6 @@
 package org.confluence.mod.common.item.shovel;
 
+import com.google.common.base.Suppliers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
@@ -17,7 +18,6 @@ import org.confluence.mod.common.init.ModTiers;
 import org.confluence.mod.common.init.block.NatureBlocks;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Supplier;
 
@@ -29,59 +29,63 @@ import java.util.function.Supplier;
  */
 public class GraveDiggersShovel extends BaseShovelItem {
 
-    // 需要 2 次敲击的方块
-    private static final Set<Block> FAST_BLOCKS_2_HITS = new HashSet<>();
-
-    // 需要 4 次敲击的方块
-    private static final Set<Block> FAST_BLOCKS_4_HITS = new HashSet<>();
-
-    static {
-        // 2 次敲击的方块（1秒）
+    /**
+     * 需要 2 次敲击的方块（1秒）
+     */
+    private static final Supplier<Set<Block>> FAST_BLOCKS_2_HITS = Suppliers.memoize(() -> {
+        Set<Block> set = new java.util.HashSet<>();
         // 原版方块
-        FAST_BLOCKS_2_HITS.add(Blocks.DIRT);
-        FAST_BLOCKS_2_HITS.add(Blocks.GRASS_BLOCK);
-        FAST_BLOCKS_2_HITS.add(Blocks.SAND);
-        FAST_BLOCKS_2_HITS.add(Blocks.SNOW_BLOCK);
-        FAST_BLOCKS_2_HITS.add(Blocks.CLAY);
-        FAST_BLOCKS_2_HITS.add(Blocks.MUD);
+        set.add(Blocks.DIRT);
+        set.add(Blocks.GRASS_BLOCK);
+        set.add(Blocks.SAND);
+        set.add(Blocks.SNOW_BLOCK);
+        set.add(Blocks.CLAY);
+        set.add(Blocks.MUD);
 
         // Terraria 方块 - 灰烬相关
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.ASH_BLOCK.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.ASH_GRASS_BLOCK.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.ASH_GRASS.get());
+        set.add(NatureBlocks.ASH_BLOCK.get());
+        set.add(NatureBlocks.ASH_GRASS_BLOCK.get());
+        set.add(NatureBlocks.ASH_GRASS.get());
 
         // Terraria 方块 - 腐化相关
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.CORRUPT_GRASS_BLOCK.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.CORRUPT_GRASS.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.CORRUPT_JUNGLE_GRASS_BLOCK.get());
+        set.add(NatureBlocks.CORRUPT_GRASS_BLOCK.get());
+        set.add(NatureBlocks.CORRUPT_GRASS.get());
+        set.add(NatureBlocks.CORRUPT_JUNGLE_GRASS_BLOCK.get());
 
         // Terraria 方块 - 猩红相关
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.CRIMSON_GRASS_BLOCK.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.CRIMSON_GRASS.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.CRIMSON_JUNGLE_GRASS_BLOCK.get());
+        set.add(NatureBlocks.CRIMSON_GRASS_BLOCK.get());
+        set.add(NatureBlocks.CRIMSON_GRASS.get());
+        set.add(NatureBlocks.CRIMSON_JUNGLE_GRASS_BLOCK.get());
 
         // Terraria 方块 - 神圣相关
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.HALLOW_GRASS_BLOCK.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.HALLOW_GRASS.get());
+        set.add(NatureBlocks.HALLOW_GRASS_BLOCK.get());
+        set.add(NatureBlocks.HALLOW_GRASS.get());
 
         // Terraria 方块 - 丛林草
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.JUNGLE_GRASS_BLOCK.get());
+        set.add(NatureBlocks.JUNGLE_GRASS_BLOCK.get());
 
         // Terraria 方块 - 蘑菇草
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.MUSHROOM_GRASS_BLOCK.get());
+        set.add(NatureBlocks.MUSHROOM_GRASS_BLOCK.get());
 
         // Terraria 方块 - 泥沙
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.SILT_BLOCK.get());
-        FAST_BLOCKS_2_HITS.add(NatureBlocks.SLUSH.get());
-    }
+        set.add(NatureBlocks.SILT_BLOCK.get());
+        set.add(NatureBlocks.SLUSH.get());
 
-    static {
-        // 4 次敲击的方块（2秒）
-        FAST_BLOCKS_4_HITS.add(NatureBlocks.EBONSAND.get());
-        FAST_BLOCKS_4_HITS.add(NatureBlocks.CRIMSAND.get());
-        FAST_BLOCKS_4_HITS.add(NatureBlocks.PEARLSAND.get());
+        return set;
+    });
+
+    /**
+     * 需要 4 次敲击的方块（2秒）
+     * 使用 Supplier 延迟初始化，避免类加载时触发 NatureBlocks 的循环依赖
+     */
+    private static final Supplier<Set<Block>> FAST_BLOCKS_4_HITS = Suppliers.memoize(() -> {
+        Set<Block> set = new java.util.HashSet<>();
+        set.add(NatureBlocks.EBONSAND.get());
+        set.add(NatureBlocks.CRIMSAND.get());
+        set.add(NatureBlocks.PEARLSAND.get());
         // Shell Pile 和 The Dirtiest Block 需要后续添加
-    }
+        return set;
+    });
 
     private static final Tier GRAVE_DIGGERS_TIER = new ModTiers.PoweredTier(
             40, // 镐力 12
@@ -117,10 +121,10 @@ public class GraveDiggersShovel extends BaseShovelItem {
     public float getDestroySpeed(@NotNull ItemStack stack, @NotNull BlockState state) {
         Block block = state.getBlock();
 
-        if (FAST_BLOCKS_2_HITS.contains(block)) {
+        if (FAST_BLOCKS_2_HITS.get().contains(block)) {
             // 2 次敲击，原速度 * 3
             return super.getDestroySpeed(stack, state) * 3.0F;
-        } else if (FAST_BLOCKS_4_HITS.contains(block)) {
+        } else if (FAST_BLOCKS_4_HITS.get().contains(block)) {
             // 4 次敲击，原速度 * 1.5
             return super.getDestroySpeed(stack, state) * 1.5F;
         }

d5fd40bfff335b3bbe450b963ab29ab03830f93e
magicharp <2749380016@qq.com>
2026-09-22 11:51:48 +0800

    平衡锤

--- ConfluenceOtherworld/src/main/java/org/confluence/mod/common/data/gen/recipe/ModRecipeProvider.java
+++ ConfluenceOtherworld/src/main/java/org/confluence/mod/common/data/gen/recipe/ModRecipeProvider.java
@@ -335,6 +335,7 @@ public class ModRecipeProvider extends AbstractRecipeProvider {
         fletchingTable(writer, ArrowItems.FLY_FISH_ARROW.toStack(10), Ingredient.of(MaterialItems.FILAMENTOUS_FIN), AmountIngredient.of(10, Items.ARROW), Ingredient.of());
         fletchingTable(writer, ArrowItems.FOSSIL_ARROW.toStack(25), Ingredient.of(), AmountIngredient.of(25, Items.ARROW), Ingredient.of(MaterialItems.STURDY_FOSSIL));
         fletchingTable(writer, ArrowItems.HELLFIRE_ARROW.toStack(64), Ingredient.of(), AmountIngredient.of(64, Items.ARROW), Ingredient.of(MaterialItems.HELLSTONE_INGOT));
+        fletchingTable(writer, ArrowItems.FROSTBURN_ARROW.toStack(64), Ingredient.of(), AmountIngredient.of(64, Items.ARROW), Ingredient.of(MaterialItems.WINTER_MARROW));
         fletchingTable(writer, ArrowItems.STAR_ARROW.toStack(10), Ingredient.of(), AmountIngredient.of(10, Items.ARROW), Ingredient.of(MaterialItems.FALLING_STAR));
         fletchingTable(writer, ArrowItems.UNHOLY_ARROW.toStack(5), Ingredient.of(), AmountIngredient.of(5, Items.ARROW), Ingredient.of(ModTags.Items.EVIL_MATERIAL));
 
